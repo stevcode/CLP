765a099782d36218cf74677e1dd25214d4db0295
steve <stev.other@gmail.com>
2011-09-20 10:28:12 -0400

    Populated Images folder

---
+++ Classroom Learning Partner/ViewModel/RibbonViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Classroom_Learning_Partner.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm/getstarted
+    /// </para>
+    /// </summary>
+    public class RibbonViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the RibbonViewModel class.
+        /// </summary>
+        public RibbonViewModel()
+        {
+        }
+    }
+}
