fb671cd104435990a2e7bb07e438ae269da7db22
steve <stev.other@gmail.com>
2011-09-20 05:39:36 -0400

    Initial Classroom Learning Partner Setup

---
+++ Classroom Learning Partner/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Classroom_Learning_Partner.ViewModel;
+
+namespace Classroom_Learning_Partner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
