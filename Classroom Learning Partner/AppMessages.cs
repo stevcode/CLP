5c21e846741b85a56617691614538be7597b0633
steve <steve@test.com>
2011-10-16 04:25:03 -0400

    Added Messaging System

---
+++ Classroom Learning Partner/AppMessages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Classroom_Learning_Partner
+{
+    public static class AppMessages
+    {
+        enum MessageTypes
+        {
+            SelectNotebook
+        }
+
+        public static class SelectNotebookMessage
+        {
+            public static void Send(string notebookName)
+            {
+                Messenger.Default.Send(notebookName, MessageTypes.SelectNotebook);
+            }
+
+            public static void Register(object recipient, Action<string> action)
+            {
+                Messenger.Default.Register(recipient, MessageTypes.SelectNotebook, action);
+            }
+        }
+
+
+    }
+}
---
+++ Classroom Learning Partner/ViewModels/NotebookSelectorViewModel.cs
@@ -0,0 +1,63 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Classroom_Learning_Partner.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm/getstarted
+    /// </para>
+    /// </summary>
+    public class NotebookSelectorViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the NotebookSelectorViewModel class.
+        /// </summary>
+        public NotebookSelectorViewModel(string notebookName)
+        {
+            _notebookName = notebookName;
+        }
+
+        #region Bindings
+
+        private string _notebookName = "";
+
+        /// <summary>
+        /// Sets and gets the NotebookName property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string NotebookName
+        {
+            get
+            {
+                return _notebookName;
+            }
+        }
+
+        #endregion //Bindings
+
+        private RelayCommand _selectNotebook;
+
+        /// <summary>
+        /// Gets the SelectNotebookCommand.
+        /// </summary>
+        public RelayCommand SelectNotebookCommand
+        {
+            get
+            {
+                return _selectNotebook
+                    ?? (_selectNotebook = new RelayCommand(
+                                          () =>
+                                          {
+                                              AppMessages.SelectNotebookMessage.Send(NotebookName);
+                                          }));
+            }
+        }
+
+    }
+}
