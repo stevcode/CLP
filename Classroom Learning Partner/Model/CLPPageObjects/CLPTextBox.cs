8b13d5e523b9f9132b2f580a812fc0fbe89a315c
steve <steve@test.com>
2011-11-08 17:07:10 -0500

    fix ratio picture  math

---
+++ Classroom Learning Partner/Model/CLPPageObjects/CLPTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Classroom_Learning_Partner.Model.CLPPageObjects
+{
+    [Serializable]
+    public class CLPTextBox : CLPPageObjectBase
+    {
+        public CLPTextBox() : this("")
+        {
+        }
+
+        public CLPTextBox(string s)
+        {
+            _text = s;
+            Position = new Point(100, 100);
+            Height = 200;
+            Width = 200;
+        }
+
+        private string _text;
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+            }
+        }
+    }
+}
--- Classroom Learning Partner/Model/CLPServiceAgent.cs
+++ Classroom Learning Partner/Model/CLPServiceAgent.cs
@@ -214,6 +214,10 @@ namespace Classroom_Learning_Partner.Model
                 {
                     pageObjectViewModel = new CLPImageStampViewModel(pageObject as CLPImageStamp);
                 }
+                else if (pageObject is CLPTextBox)
+                {
+                    pageObjectViewModel = new CLPTextBoxViewModel(pageObject as CLPTextBox);
+                }
                 else
                 {
                     pageObjectViewModel = null;
