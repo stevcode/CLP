3437da73aecf22bbd5ffe868fda3ddfaee22ef2b
Ernie Park <eipark@mit.edu>
2011-12-12 16:04:47 -0500

    make a few performance improvements on laser, still slow

--- Classroom Learning Partner/Views/CLPPageView.xaml.cs
+++ Classroom Learning Partner/Views/CLPPageView.xaml.cs
@@ -47,6 +47,12 @@ namespace Classroom_Learning_Partner.Views
                 else RootGrid.MouseMove -= sendLaserPointerPosition;
             });
 
+            if (App.CurrentUserMode == App.UserMode.Projector)
+            {
+                RootGrid.Children.Add(_laserPoint);
+                _laserPoint.Visibility = Visibility.Collapsed;
+            }
+
             //Register so we receive mouse coordinates for the laser on the projector
             AppMessages.UpdateLaserPointerPosition.Register(this, (pt) =>
             {
@@ -115,9 +121,6 @@ namespace Classroom_Learning_Partner.Views
                 
                 return HitTestResultBehavior.Continue;
             }
-
-            
-            
         }
 
         void timer_Tick(object sender, EventArgs e)
@@ -128,6 +131,7 @@ namespace Classroom_Learning_Partner.Views
         }
 
         private LaserPoint _laserPoint = new LaserPoint();
+        private Thickness _laserPointMargins = new Thickness();
         public void updateLaserPointerPosition(Point pt)
         {
             // We cannot update the UI element directly, need to access it using the UI thread so we have this
@@ -144,9 +148,11 @@ namespace Classroom_Learning_Partner.Views
         // Does the actual updating of the LaserPoint
         private void setUILaserPointerValue(Point pt)
         {
-            if (RootGrid.Children.Contains(_laserPoint)) RootGrid.Children.Remove(_laserPoint);
-            RootGrid.Children.Add(_laserPoint);
-            _laserPoint.RootGrid.Margin = new Thickness(pt.X, pt.Y, 0, 0);
+            //if (RootGrid.Children.Contains(_laserPoint)) RootGrid.Children.Remove(_laserPoint);
+            _laserPoint.Visibility = Visibility.Visible;
+            _laserPointMargins.Left = pt.X;
+            _laserPointMargins.Top = pt.Y;
+            _laserPoint.RootGrid.Margin = _laserPointMargins;
         }
 
         private void sendLaserPointerPosition(object sender, MouseEventArgs e)
@@ -156,7 +162,7 @@ namespace Classroom_Learning_Partner.Views
                 Point pt = e.GetPosition(this.RootGrid);
                 if (pt.X > 816) pt.X = 816;
                 if (pt.Y > 1056) pt.Y = 1056;
-                CLPService.SendLaserPosition(e.GetPosition(this.RootGrid));
+                CLPService.SendLaserPosition(pt);
             }
         }
 
