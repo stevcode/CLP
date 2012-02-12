b9d8caa22bb40b27c33b12174c2e3efb6003f7c9
steve <steve@test.com>
2012-02-12 10:08:47 -0500

    CLPPagePreviewV converted to catel

--- Classroom Learning Partner/Views/CLPPagePreviewView.xaml.cs
+++ Classroom Learning Partner/Views/CLPPagePreviewView.xaml.cs
@@ -1,23 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Catel.Windows.Controls;
+using Classroom_Learning_Partner.ViewModels;
 
 namespace Classroom_Learning_Partner.Views
 {
     /// <summary>
     /// Interaction logic for CLPPagePreviewView.xaml
     /// </summary>
-    public partial class CLPPagePreviewView : UserControl
+    public partial class CLPPagePreviewView : UserControl<CLPPageViewModel>
     {
         public CLPPagePreviewView()
         {
