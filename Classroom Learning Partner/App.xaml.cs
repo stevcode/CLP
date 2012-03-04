319cd427d8fa12227f7aa6d3d10d367650c0bab4
steve <steve>
2012-03-04 13:59:32 -0500

    externalized some dlls

--- Classroom Learning Partner/App.xaml.cs
+++ Classroom Learning Partner/App.xaml.cs
@@ -35,7 +35,7 @@ namespace Classroom_Learning_Partner
             base.OnStartup(e);
             CLPService = new CLPServiceAgent();
 
-            CurrentUserMode = UserMode.Instructor;
+            CurrentUserMode = UserMode.Projector;
             _databaseUse = DatabaseMode.NotUsing;
             if (_databaseUse == DatabaseMode.Using && App.CurrentUserMode == UserMode.Server)
             {
