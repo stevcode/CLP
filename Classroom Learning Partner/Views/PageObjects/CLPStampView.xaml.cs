7f72b806f26fba251cb7afb03e1cf298149c9c3e
steve <stev.other@gmail.com>
2012-03-18 04:22:38 -0400

    stampView added

---
+++ Classroom Learning Partner/Views/PageObjects/CLPStampView.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Classroom_Learning_Partner.Views.PageObjects
+{
+    using Catel.Windows.Controls;
+    using Classroom_Learning_Partner.ViewModels.PageObjects;
+
+    /// <summary>
+    /// Interaction logic for CLPStampView.xaml.
+    /// </summary>
+    public partial class CLPStampView : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CLPStampView"/> class.
+        /// </summary>
+        public CLPStampView()
+        {
+            InitializeComponent();
+            SkipSearchingForInfoBarMessageControl = true;
+        }
+
+        protected override System.Type GetViewModelType()
+        {
+            return typeof(CLPStampViewModel);
+        }
+    }
+}
