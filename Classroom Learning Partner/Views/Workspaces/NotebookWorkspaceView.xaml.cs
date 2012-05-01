611b5b5d24567ff9934be9e59ee2177699ab7849
steve <steve>
2012-04-30 20:13:06 -0400

    notebook pages selecting finalized.
    submissions sidebar can collapse now.

--- Classroom Learning Partner/Views/Workspaces/NotebookWorkspaceView.xaml.cs
+++ Classroom Learning Partner/Views/Workspaces/NotebookWorkspaceView.xaml.cs
@@ -2,6 +2,10 @@
 using Classroom_Learning_Partner.ViewModels;
 using System;
 using Classroom_Learning_Partner.ViewModels.Displays;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Classroom_Learning_Partner.Model;
+using System.Windows;
 
 namespace Classroom_Learning_Partner.Views.Workspaces
 {
@@ -22,6 +26,32 @@ namespace Classroom_Learning_Partner.Views.Workspaces
         protected override System.Type GetViewModelType()
         {
             return typeof(NotebookWorkspaceViewModel);
+        }
+
+        private ToggleButton currentToggledButton = null;
+        private void ToggleButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ToggleButton button = sender as ToggleButton;
+            if (currentToggledButton != null && currentToggledButton != button)
+            {
+                currentToggledButton.IsChecked = false;
+            }
+            currentToggledButton = button;
+            if ((bool)currentToggledButton.IsChecked)
+            {
+                SubmissionsSideBar.Visibility = Visibility.Visible;
+                CLPPage page = (((((sender as ToggleButton).Parent as Grid).Parent as Grid).Children[0] as Border).Child as ContentPresenter).Content as CLPPage;
+                string pageID = page.UniqueID;
+                var viewModel = this.ViewModel as NotebookWorkspaceViewModel;
+                if (viewModel.Notebook.Submissions.ContainsKey(pageID))
+                {
+                    viewModel.SubmissionPages = viewModel.Notebook.Submissions[pageID];
+                }
+            }
+            else
+            {
+                SubmissionsSideBar.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
