df7a69f5c5b056ee79bbfd92678422a4d774b5da
steve <steve>
2012-05-19 18:51:23 -0400

    created ICLPDisplay interface

---
+++ Classroom Learning Partner/Model/Displays/ACLPDisplayBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using Catel.Data;
+
+namespace Classroom_Learning_Partner.Model.Displays
+{
+    public enum DisplayTypes
+    {
+        Mirror,
+        Grid,
+        Column,
+        Canvas
+    }
+
+    public interface ICLPDisplay
+    {
+        string UniqueID;
+        DisplayTypes DisplayType;
+        string ParentNotebookUniqueID;
+        DateTime CreationDate;
+        DateTime LastProjectionTime;
+        List<DateTime> ProjectionTimesHistory;
+
+        ObservableCollection<CLPPage> ForeignPages;
+        ObservableCollection<Tuple<bool,string>> DisplayPages;
+    }
+
+    abstract public class ACLPDisplayBase : DataObjectBase<ACLPDisplayBase>, ICLPDisplay
+    {
+    }
+}
