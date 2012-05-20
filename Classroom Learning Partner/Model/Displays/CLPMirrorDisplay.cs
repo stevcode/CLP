87e46d8ec71d832d132ee0a65f3efbec809a6aa9
steve <steve>
2012-05-20 07:32:10 -0400

    Created Mirror and Grid Display Models

---
+++ Classroom Learning Partner/Model/Displays/CLPMirrorDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Classroom_Learning_Partner.Model.Displays
+{
+    public class CLPMirrorDisplay : ACLPDisplayBase
+    {
+        #region Constructor & destructor
+        /// <summary>
+        /// Initializes a new object from scratch.
+        /// </summary>
+        public CLPMirrorDisplay()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new object based on <see cref="SerializationInfo"/>.
+        /// </summary>
+        /// <param name="info"><see cref="SerializationInfo"/> that contains the information.</param>
+        /// <param name="context"><see cref="StreamingContext"/>.</param>
+        protected CLPMirrorDisplay(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+        #endregion
+
+        public DisplayTypes DisplayType { get { return DisplayTypes.Mirror; } }
+    }
+}
