7e8fb2a288241c0434ab2e97c8a80b157c252e8f
steve <steve>
2012-05-31 13:51:02 -0400

    cleaned up UserLoginView

--- Classroom Learning Partner/App.xaml.cs
+++ Classroom Learning Partner/App.xaml.cs
@@ -48,7 +48,7 @@ namespace Classroom_Learning_Partner
             //Stops Catel UserControls from searching for InfoBar (not being used for this project, massive time consumer)
             Catel.Windows.Controls.UserControl.DefaultSkipSearchingForInfoBarMessageControlValue = true;
 
-            CurrentUserMode = UserMode.Instructor;
+            CurrentUserMode = UserMode.Student;
             _databaseUse = DatabaseMode.Using;
 
             Logger.Instance.InitializeLog();
