87c94d87ac71b5006088192f8de0e5510ab10547
steve <steve>
2012-08-19 04:57:31 -0400

    Refactored BindableRichTextBox into it's own class

---
+++ Classroom Learning Partner/Views/Custom Controls/BindableRichTextBox.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Classroom_Learning_Partner.Views
+{
+    public class BindableRichTextBox : RichTextBox
+    {
+        private HashSet<Thread> _recursionProtection = new HashSet<Thread>();
+
+        public BindableRichTextBox()
+        {
+            this.TextChanged += BindableRTB_TextChanged;
+            BorderThickness = new Thickness(0.0);
+        }
+
+        void BindableRTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextRange text = new TextRange(this.Document.ContentStart, this.Document.ContentEnd);
+            MemoryStream stream = new MemoryStream();
+            text.Save(stream, DataFormats.Rtf);
+
+            this.RTFText = Encoding.UTF8.GetString(stream.ToArray());
+        }
+
+        public string RTFText
+        {
+            get { return (string)GetValue(RTFTextProperty); }
+            set
+            {
+                _recursionProtection.Add(Thread.CurrentThread);
+                SetValue(RTFTextProperty, value);
+                _recursionProtection.Remove(Thread.CurrentThread);
+            }
+        }
+
+        public static readonly DependencyProperty RTFTextProperty = DependencyProperty.RegisterAttached(
+            "RTFText",
+            typeof(string),
+            typeof(BindableRichTextBox),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, CallBack)
+        );
+
+        private static void CallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            BindableRichTextBox rtb = d as BindableRichTextBox;
+
+            if(rtb._recursionProtection.Contains(Thread.CurrentThread))
+                return;
+
+            try
+            {
+                string rtfText = e.NewValue as string;
+                var doc = new FlowDocument();
+
+                if(rtfText == "")
+                {
+                    //create rtf version of ""
+                    //RichTextBox richTextBox = new RichTextBox();
+                    //richTextBox.Text = "Européen";
+                    //string rtfFormattedString = richTextBox.Rtf;
+                }
+
+                var bytes = Encoding.UTF8.GetBytes(rtfText);
+                var stream = new MemoryStream(bytes);
+
+                var range = new TextRange(doc.ContentStart, doc.ContentEnd);
+                range.Load(stream, DataFormats.Rtf);
+
+                // Set the document
+                rtb.Document = doc;
+            }
+            catch(Exception)
+            {
+                rtb.Document = new FlowDocument();
+            }
+        }
+    }
+}
--- Classroom Learning Partner/Views/PageObjects/CLPTextBoxView.xaml.cs
+++ Classroom Learning Partner/Views/PageObjects/CLPTextBoxView.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -14,79 +10,6 @@ using Classroom_Learning_Partner.ViewModels;
 
 namespace Classroom_Learning_Partner.Views
 {
-    public class BindableRTB : RichTextBox
-    {
-        private HashSet<Thread> _recursionProtection = new HashSet<Thread>();
-
-        public BindableRTB()
-        {
-            this.TextChanged += BindableRTB_TextChanged;
-            BorderThickness = new Thickness(0.0);
-        }
-
-        void BindableRTB_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextRange text = new TextRange(this.Document.ContentStart, this.Document.ContentEnd);
-            MemoryStream stream = new MemoryStream();
-            text.Save(stream, DataFormats.Rtf);
-
-            this.RTFText = Encoding.UTF8.GetString(stream.ToArray());
-        }
-
-        public string RTFText
-        {
-            get { return (string)GetValue(RTFTextProperty); }
-            set
-            {
-                _recursionProtection.Add(Thread.CurrentThread);
-                SetValue(RTFTextProperty, value);
-                _recursionProtection.Remove(Thread.CurrentThread);
-            }
-        }
-
-        public static readonly DependencyProperty RTFTextProperty = DependencyProperty.RegisterAttached(
-            "RTFText",
-            typeof(string),
-            typeof(BindableRTB),
-            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, CallBack)
-        );
-
-        private static void CallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            BindableRTB rtb = d as BindableRTB;
-
-            if(rtb._recursionProtection.Contains(Thread.CurrentThread))
-                return;
-
-            try
-            {
-                string rtfText = e.NewValue as string;
-                var doc = new FlowDocument();
-
-                if(rtfText == "")
-                {
-                    //create rtf version of ""
-                    //RichTextBox richTextBox = new RichTextBox();
-                    //richTextBox.Text = "Européen";
-                    //string rtfFormattedString = richTextBox.Rtf;
-                }
-
-                var bytes = Encoding.UTF8.GetBytes(rtfText);
-                var stream = new MemoryStream(bytes);
-
-                var range = new TextRange(doc.ContentStart, doc.ContentEnd);
-                range.Load(stream, DataFormats.Rtf);
-
-                // Set the document
-                rtb.Document = doc;
-            }
-            catch(Exception)
-            {
-                rtb.Document = new FlowDocument();
-            }
-        }
-    }
-
     /// <summary>
     /// Interaction logic for CLPTextBoxView.xaml
     /// </summary>
