a027dc037ee147c7af1ef26676f4152fec2f9391
steve <steve>
2012-09-25 09:16:33 -0400

    Correctly Collapsing Sidebars.

--- Classroom Learning Partner/Views/Workspaces/NotebookWorkspaceView.xaml.cs
+++ Classroom Learning Partner/Views/Workspaces/NotebookWorkspaceView.xaml.cs
@@ -26,6 +26,13 @@ namespace Classroom_Learning_Partner.Views
         public NotebookWorkspaceView()
         {
             InitializeComponent();
+
+            submissionColumnWidth = SubmissionColumn.Width.Value;
+            submissionColumnMinWidth = SubmissionColumn.MinWidth;
+
+            SubmissionColumn.MinWidth = 0;
+            SubmissionColumn.Width = new GridLength(0);
+            SubmissionSplitterColumn.Width = new GridLength(0);
         }
 
         protected override System.Type GetViewModelType()
@@ -48,6 +55,9 @@ namespace Classroom_Learning_Partner.Views
                 FilterSideBar.Visibility = Visibility.Visible;
                 SubmissionPagesSplitter.Visibility = Visibility.Visible;
                 submissionsVisibility = Visibility.Visible;
+                SubmissionColumn.MinWidth = submissionColumnMinWidth;
+                SubmissionColumn.Width = new GridLength(submissionColumnWidth);
+                SubmissionSplitterColumn.Width = new GridLength(5);
                 CLPPage page = (((((sender as ToggleButton).Parent as Grid).Parent as Grid).Children[1] as Border).Child as ContentPresenter).Content as CLPPage;
                 string pageID = page.UniqueID;
                 var viewModel = this.ViewModel as NotebookWorkspaceViewModel;
@@ -62,6 +72,12 @@ namespace Classroom_Learning_Partner.Views
                 FilterSideBar.Visibility = Visibility.Collapsed;
                 SubmissionPagesSplitter.Visibility = Visibility.Collapsed;
                 submissionsVisibility = Visibility.Collapsed;
+                submissionColumnWidth = SubmissionColumn.Width.Value;
+                submissionColumnMinWidth = SubmissionColumn.MinWidth;
+
+                SubmissionColumn.MinWidth = 0;
+                SubmissionColumn.Width = new GridLength(0);
+                SubmissionSplitterColumn.Width = new GridLength(0);
                 (((sender as ToggleButton).Parent as Grid).Parent as Grid).Background = new SolidColorBrush(Colors.Transparent);
             }
 
@@ -89,6 +105,10 @@ namespace Classroom_Learning_Partner.Views
         }
 
         private Visibility submissionsVisibility = Visibility.Collapsed;
+        private double submissionColumnWidth;
+        private double submissionColumnMinWidth;
+        private double notebookPageColumnWidth;
+        private double notebookPageColumnMinWidth;
 
         private void ShowNotebookPagesToggleButton_Click(object sender, RoutedEventArgs e)
         {
@@ -97,10 +117,21 @@ namespace Classroom_Learning_Partner.Views
                 NotebookPagesBorder.Visibility = Visibility.Visible;
                 NotebookPagesSplitter.Visibility = Visibility.Visible;
 
+                NotebookPageColumn.MinWidth = notebookPageColumnMinWidth;
+                NotebookPageColumn.Width = new GridLength(notebookPageColumnWidth);
+                NotebookPageSplitterColumn.Width = new GridLength(5);
+
                 if(MainWindow.Ribbon.InstructorVisibility == Visibility.Visible)
                 {
                     FilterSideBar.Visibility = submissionsVisibility;
                     SubmissionPagesSplitter.Visibility = submissionsVisibility;
+
+                    if(submissionsVisibility == Visibility.Visible)
+                    {
+                        SubmissionColumn.MinWidth = submissionColumnMinWidth;
+                        SubmissionColumn.Width = new GridLength(submissionColumnWidth);
+                        SubmissionSplitterColumn.Width = new GridLength(5);
+                    }
                 }
             }
             else
@@ -108,10 +139,27 @@ namespace Classroom_Learning_Partner.Views
                 NotebookPagesBorder.Visibility = Visibility.Collapsed;
                 NotebookPagesSplitter.Visibility = Visibility.Collapsed;
 
+                notebookPageColumnWidth = NotebookPageColumn.Width.Value;
+                notebookPageColumnMinWidth = NotebookPageColumn.MinWidth;
+
+                NotebookPageColumn.MinWidth = 0;
+                NotebookPageColumn.Width = new GridLength(0);
+                NotebookPageSplitterColumn.Width = new GridLength(0);
+
                 if(MainWindow.Ribbon.InstructorVisibility == Visibility.Visible)
                 {
                     FilterSideBar.Visibility = Visibility.Collapsed;
                     SubmissionPagesSplitter.Visibility = Visibility.Collapsed;
+
+                    if(submissionsVisibility == Visibility.Visible)
+                    {
+                        submissionColumnWidth = SubmissionColumn.Width.Value;
+                        submissionColumnMinWidth = SubmissionColumn.MinWidth;
+
+                        SubmissionColumn.MinWidth = 0;
+                        SubmissionColumn.Width = new GridLength(0);
+                        SubmissionSplitterColumn.Width = new GridLength(0);
+                    }
                 }
             }
         }
