a5e974d65d2a7b9e9a48218c4589f9f04c629dd1
steve <steve>
2012-10-03 15:47:56 -0400

    Audio cleanup.
    Added Base.Net.dll Sound library

--- CLP.Models/CLPPageObjects/CLPAudio.cs
+++ CLP.Models/CLPPageObjects/CLPAudio.cs
@@ -10,33 +10,19 @@ using System.Collections.Generic;
 namespace CLP.Models
 {
     [Serializable]
-    [AllowNonSerializableMembers]
     public class CLPAudio : CLPPageObjectBase
     {
+        #region Constructor
 
-        #region Constructor & destructor
         /// <summary>
         /// Initializes a new object from scratch.
         /// </summary>
-        public CLPAudio(string ID, CLPPage page)
+        public CLPAudio(CLPPage page)
             : base(page)
         {
-            //string fullPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Audio_Files\" + path + ".mp3";
-            //if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Audio_Files"))
-            //{
-            //    DirectoryInfo worked = Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Audio_Files\");
-            //}
-            //int i = 0;
-            //while (File.Exists(fullPath))
-            //{
-            //    fullPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Audio_Files\" + path + i.ToString() + ".mp3";
-            //    i++;
-            //}
-            this.File = new Byte[0];
-            this.ID = ID;
+            ByteSource = new Byte[0];
             Height = 70;
             Width = 200;
-            CanAcceptStrokes = false;
         }
 
         /// <summary>
@@ -47,38 +33,22 @@ namespace CLP.Models
         protected CLPAudio(SerializationInfo info, StreamingContext context)
             : base(info, context) { }
 
-        #endregion
+        #endregion //Constructor
 
         #region Properties
-
-       
-       public string FilePath
-        {
-            get { return GetValue<string>(FilePathProperty); }
-            private set { SetValue(FilePathProperty, value); }
-        }
-
-        public static readonly PropertyData FilePathProperty = RegisterProperty("FilePath", typeof(string), null);
         
-        public byte[] File
+        public byte[] ByteSource
         {
-            get { return GetValue<byte[]>(FileProperty); }
-            set { SetValue(FileProperty, value); }
+            get { return GetValue<byte[]>(ByteSourceProperty); }
+            set { SetValue(ByteSourceProperty, value); }
         }
 
-        public static readonly PropertyData FileProperty = RegisterProperty("File", typeof(byte[]), null);
-
-        public string ID
-        {
-            get { return GetValue<string>(IDProperty); }
-            set { SetValue(IDProperty, value); }
-        }
-        public static readonly PropertyData IDProperty = RegisterProperty("ID", typeof(string), null);
-
+        public static readonly PropertyData ByteSourceProperty = RegisterProperty("ByteSource", typeof(byte[]), () => new Byte[0]);
 
         #endregion
 
-        #region methods
+        #region Methods
+
         public override string PageObjectType
         {
             get { return "CLPAudio"; }
@@ -89,52 +59,6 @@ namespace CLP.Models
             return this.Duplicate();
         }
 
-        public static void ConvertWavMP3(string wavFile, string outmp3File)
-        {
-            if(System.IO.File.Exists(outmp3File))
-            {
-                System.IO.File.Delete(outmp3File);
-            }
-	        ProcessStartInfo psi = new ProcessStartInfo();
-	        psi.FileName = @"External Libraries\lame.exe";
-	        psi.Arguments = "-b 20 " + wavFile + " " + outmp3File;
-	        psi.WindowStyle = ProcessWindowStyle.Hidden;
-	        Process p = Process.Start(psi);
-	        p.WaitForExit();
-        }
-
-        public static byte[] FileToByteArray(string _FileName)
-        {
-            byte[] _Buffer = null;
-
-            try
-            {
-                // Open file for reading
-                System.IO.FileStream _FileStream = new System.IO.FileStream(_FileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
-
-                // attach filestream to binary reader
-                System.IO.BinaryReader _BinaryReader = new System.IO.BinaryReader(_FileStream);
-
-                // get total byte length of the file
-                long _TotalBytes = new System.IO.FileInfo(_FileName).Length;
-
-                // read entire file into buffer
-                _Buffer = _BinaryReader.ReadBytes((Int32)_TotalBytes);
-
-                // close file reader
-                _FileStream.Close();
-                _FileStream.Dispose();
-                _BinaryReader.Close();
-            }
-            catch (Exception _Exception)
-            {
-                // Error
-                Console.WriteLine("Exception caught in process: {0}", _Exception.ToString());
-            }
-
-            return _Buffer;
-        }
         #endregion
-
     }
 }
