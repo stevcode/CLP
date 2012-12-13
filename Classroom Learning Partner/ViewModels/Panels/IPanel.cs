7117afce5366b58fb7f9fc7b8ad87dd0717de1b3
steve <steve>
2012-12-13 14:06:35 -0500

    Start of some ViewModel Interfaces

---
+++ Classroom Learning Partner/ViewModels/Panels/IPanel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom_Learning_Partner.ViewModels
+{
+    public enum PanelLocation
+    {
+        Left,
+        Right,
+        Top,
+        Bottom,
+        Floating
+    }
+
+    public interface IPanel
+    {
+        bool IsPinned { get; set; }
+        bool IsVisible { get; set; }
+        PanelLocation Location { get; set; }
+    }
+}
