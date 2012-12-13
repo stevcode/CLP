efc5932caeeebd3cd7a5cca79fe050c7357988a6
steve <steve>
2012-12-13 13:40:23 -0500

    Added Interfaces for PageObjects for future use.

--- CLP.Models/CLPPageObjects/PageObject Adorners/ICLPPageObject.cs
+++ CLP.Models/CLPPageObjects/PageObject Adorners/ICLPPageObject.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Windows;
 using System.Windows.Ink;
 
 namespace CLP.Models
---
+++ CLP.Models/CLPPageObjects/PageObject Adorners/IInkStrokeAccepter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Ink;
+
+namespace CLP.Models
+{
+    public interface IInkStrokeAccepter
+    {
+        bool CanAcceptStrokes { get; set; }
+        ObservableCollection<string> AcceptedStrokeParentIDs { get; set; }
+        
+        void AcceptStrokes(StrokeCollection addedStrokes, StrokeCollection removedStrokes);
+        void RefreshStrokeParentIDs();
+        StrokeCollection GetStrokesOverThisPageObject();
+    }
+}
