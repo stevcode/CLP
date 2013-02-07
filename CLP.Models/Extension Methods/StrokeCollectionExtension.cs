4df1bd6e70bdd9f64d4a751793a5c1976ac7f42f
steve <steve>
2013-02-07 14:59:02 -0500

    StrokeCollection ExtensionMethod

---
+++ CLP.Models/Extension Methods/StrokeCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.IO;
+
+namespace CLP.Models
+{
+    public static class StrokeCollectionExtension
+    {
+        public static string SaveStrokeCollection(this StrokeCollection strokes)
+        {
+            
+
+            MemoryStream stream = new MemoryStream();
+            strokes.Save(stream, true);
+            return "";
+        }
+    }
+}
