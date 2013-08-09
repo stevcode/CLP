8c6fb230c3391dacf52bea511e4d0f6abfed92e1
Matt Peairs <peairs>
2013-08-09 15:16:56 -0400

    Attempting to build and run unit tests

---
+++ TaggingTests/TaggingTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CLP.Models;
+
+namespace TaggingTests
+{
+    [TestClass]
+    public class TaggingTests
+    {
+        [TestMethod]
+        public void InstantiateTags()
+        {
+            TagType correctness = CorrectnessTagType.Instance;
+            TagType strategy = ArrayStrategyTagType.Instance;
+        }
+    }
+}
