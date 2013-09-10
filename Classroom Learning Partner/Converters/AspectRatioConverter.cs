03117255385e5885394d9d7c666cbc25b04a4de2
steve <steve>
2013-09-10 12:14:46 -0400

    GridDisplay Tweaks

---
+++ Classroom Learning Partner/Converters/AspectRatioConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Classroom_Learning_Partner.Converters
+{
+    public class AspectRatioConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+                              object parameter, System.Globalization.CultureInfo culture)
+        {
+            var side = (double)value;
+            var aspectRatio = (double)parameter;
+            return side;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
