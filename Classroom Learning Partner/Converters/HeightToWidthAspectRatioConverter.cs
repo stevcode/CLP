cb369ad3f65546b03ed57e77c9686f9f85529f31
steve <steve>
2013-09-11 03:42:31 -0400

    Resize MirrorDisplay selection rectangle based on page aspect ratio.

---
+++ Classroom Learning Partner/Converters/HeightToWidthAspectRatioConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Classroom_Learning_Partner.Converters
+{
+    public class HeightToWidthAspectRatioConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType,
+                              object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                var height = (double)values[0];
+                var aspectRatio = (double)values[1];
+                return height*aspectRatio;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return 100.0;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes,
+                                    object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
