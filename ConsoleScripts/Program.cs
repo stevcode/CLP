9d845ea32aa1871c6e9a47441766a8d237dbeac7
Matt Peairs <peairs>
2013-10-03 16:56:37 -0400

    Added commands to Program.cs: "interpret student" and "interpret teacher", which apply analysis routines to all pages/submissions in all .clp files in StudentNotebooks and Notebooks, respectively.

--- ConsoleScripts/Program.cs
+++ ConsoleScripts/Program.cs
@@ -10,12 +10,70 @@ namespace ConsoleScripts
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                Console.Write("> ");
+                string command = Console.ReadLine();
+                if(!processCommand(command))
+                {
+                    break;
+                }
+            }
+        }
+
+        // Process a console command
+        // returns true iff the console should accept another command after this one
+        static Boolean processCommand(string command)
+        {
+            if(command.Equals("replace"))
+            {
+                replace();
+            }
+            else if(command.Equals("interpret student"))
+            {
+                batchInterpretStudent();
+            }
+            else if(command.Equals("interpret teacher"))
+            {
+                batchInterpretTeacher();
+            }
+            else if(command.Equals("strip"))
+            {
+                stripHistory();
+            }
+            else if(command.Equals("exit"))
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Command not recognized");
+            }
+            return true;
+        }
+
+        static void replace()
+        {
             Console.WriteLine("Starting");
 
-            StripHistory.StripAll();
+            NotebookMerge.Replace();
 
             Console.WriteLine("Ended");
-            Console.ReadLine();
+        }
+
+        static void batchInterpretStudent()
+        {
+            BatchInterpreter.InterpretStudentNotebooks();
+        }
+
+        static void batchInterpretTeacher()
+        {
+            BatchInterpreter.InterpretTeacherNotebooks();
+        }
+
+        static void stripHistory()
+        {
+            StripHistory.StripAll();
         }
     }
 }
