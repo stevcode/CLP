1e5e001e1683e7a5efdf463c729d8b27acc28742
liz <liz>
2014-01-12 21:58:32 -0500

    more changes for factor card class

--- Classroom Learning Partner/ViewModels/PageObjects/CLPFactorCardViewModel.cs
+++ Classroom Learning Partner/ViewModels/PageObjects/CLPFactorCardViewModel.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Catel.Data;
+using Catel.MVVM;
+using Classroom_Learning_Partner.Views.Modal_Windows;
+using CLP.Models;
 
-namespace Classroom_Learning_Partner.ViewModels.PageObjects
+namespace Classroom_Learning_Partner.ViewModels
 {
 
-    class CLPFactorCardViewModel
+    public class CLPFactorCardViewModel : CLPArrayViewModel
     {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CLPFactorCardViewModel"/> class.
+        /// </summary>
+        public CLPFactorCardViewModel(CLPFactorCard factorCard) : base(factorCard as CLPArray)
+        {
+        }
+
+        #endregion //Constructor    
+
     }
 }
--- Classroom Learning Partner/Views/PageObjects/CLPFactorCardView.xaml.cs
+++ Classroom Learning Partner/Views/PageObjects/CLPFactorCardView.xaml.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Timers;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using CLP.Models;
+using Classroom_Learning_Partner.ViewModels;
 
-namespace Classroom_Learning_Partner.Views.PageObjects
+namespace Classroom_Learning_Partner.Views
 {
     /// <summary>
     /// Interaction logic for CLPFactorCardView.xaml
     /// </summary>
-    public partial class CLPFactorCardView : Window
+    public partial class CLPFactorCardView
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CLPFactorCardView"/> class.
+        /// </summary>
         public CLPFactorCardView()
         {
             InitializeComponent();
