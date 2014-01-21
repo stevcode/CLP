527e04fd3fe28bda70d20a3a59a6f4fddf710cdb
steve <steve>
2014-01-21 12:11:05 -0500

    Fallback for submissions without thumbnails.

---
+++ Classroom Learning Partner/Converters/ByteArrayToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Classroom_Learning_Partner.Converters
+{
+    public class ByteArrayToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var byteArray = value as byte[];
+            return byteArray != null && byteArray.Length != 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
