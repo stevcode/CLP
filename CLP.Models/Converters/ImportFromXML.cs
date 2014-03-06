30dae7bb3a5a0cc1cade635a230d5333330e1340
steve <steve>
2014-03-05 22:33:29 -0500

    Start fo XML Importer

---
+++ CLP.Models/Converters/ImportFromXML.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+
+namespace CLP.Models.Converters
+{
+    public static class ImportFromXML
+    {
+        public static CLPNotebook ImportNotebook(string initialFilePath)
+        {
+            var notebook = new CLPNotebook();
+
+            var reader = new XmlTextReader(initialFilePath);
+
+            while(reader.Read())
+            {
+                
+            }
+
+
+
+
+
+
+
+            return notebook;
+        }
+    }
+}
