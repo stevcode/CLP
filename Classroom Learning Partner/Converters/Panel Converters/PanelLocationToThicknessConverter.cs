767b11bf06b77058831496fd58a8704801e67071
steve <steve>
2014-04-03 01:20:39 -0400

    StagingPanel refinement

---
+++ Classroom Learning Partner/Converters/Panel Converters/PanelLocationToThicknessConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Classroom_Learning_Partner.ViewModels;
+
+namespace Classroom_Learning_Partner.Converters
+{
+    public class PanelLocationToThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(!(parameter is double) ||
+               !(value is PanelLocation))
+            {
+                return new Thickness(0, 0, 0, 0);
+            }
+
+            var thickness = (double)parameter;
+            var location = (PanelLocation)value;
+            switch(location)
+            {
+                case PanelLocation.Left:
+                    return new Thickness(thickness, 0, 0, 0);
+                case PanelLocation.Right:
+                    return new Thickness(0, 0, thickness, 0);
+                case PanelLocation.Top:
+                    return new Thickness(0, thickness, 0, 0);
+                case PanelLocation.Bottom:
+                    return new Thickness(0, 0, 0, thickness);
+                case PanelLocation.Floating:
+                    return new Thickness(0, 0, 0, 0);
+                default:
+                    return new Thickness(0, 0, 0, 0);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+    }
+}
--- Classroom Learning Partner/Views/Panels/StagingPanelView.xaml.cs
+++ Classroom Learning Partner/Views/Panels/StagingPanelView.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Classroom_Learning_Partner.ViewModels;
 
 namespace Classroom_Learning_Partner.Views
 {
@@ -20,14 +8,8 @@ namespace Classroom_Learning_Partner.Views
     /// </summary>
     public partial class StagingPanelView
     {
-        public StagingPanelView()
-        {
-            InitializeComponent();
-        }
+        public StagingPanelView() { InitializeComponent(); }
 
-        protected override Type GetViewModelType()
-        {
-            return typeof(StagingPanelViewModel);
-        }
+        protected override Type GetViewModelType() { return typeof(StagingPanelViewModel); }
     }
 }
