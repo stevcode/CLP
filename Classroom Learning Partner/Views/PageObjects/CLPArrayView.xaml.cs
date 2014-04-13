ba79fbea6f5be298d600dfe4794eea62637ed960
steve <steve>
2014-04-13 03:42:42 -0400

    Lag free grid lines

--- Classroom Learning Partner/Views/PageObjects/CLPArrayView.xaml.cs
+++ Classroom Learning Partner/Views/PageObjects/CLPArrayView.xaml.cs
@@ -25,73 +25,73 @@ namespace Classroom_Learning_Partner.Views
 
         private void ArrayTopHitBox_MouseMove(object sender, MouseEventArgs e)
         {
-            //_divisorHideTimer.Stop();
-            //var clpArrayViewModel = ViewModel as CLPArrayViewModel;
-            //if(clpArrayViewModel == null ||
-            //   !clpArrayViewModel.IsDivisionBehaviorOn ||
-            //   clpArrayViewModel.IsDefaultAdornerVisible)
-            //{
-            //    return;
-            //}
+            _divisorHideTimer.Stop();
+            var clpArrayViewModel = ViewModel as CLPArrayViewModel;
+            if(clpArrayViewModel == null ||
+               !clpArrayViewModel.IsDivisionBehaviorOn ||
+               clpArrayViewModel.IsDefaultAdornerVisible)
+            {
+                return;
+            }
 
-            //ACLPPageBaseViewModel.ClearAdorners(clpArrayViewModel.PageObject.ParentPage);
-            //var clpArray = clpArrayViewModel.PageObject as CLPArray;
-            //if(clpArray != null)
-            //{
-            //    clpArrayViewModel.TopArrowPosition = e.GetPosition(Array).X;
-            //}
-            //if(clpArrayViewModel.TopArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.TopArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
-            //}
-            //if(clpArrayViewModel.TopArrowPosition > clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.TopArrowPosition = clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP;
-            //}
-            //clpArrayViewModel.IsTopAdornerVisible = true;
-            //clpArrayViewModel.IsLeftAdornerVisible = false;
-            //clpArrayViewModel.IsDefaultAdornerVisible = false;
-            //clpArrayViewModel.IsAdornerVisible = true;
+            ACLPPageBaseViewModel.ClearAdorners(clpArrayViewModel.PageObject.ParentPage);
+            var clpArray = clpArrayViewModel.PageObject as CLPArray;
+            if(clpArray != null)
+            {
+                clpArrayViewModel.TopArrowPosition = e.GetPosition(Array).X;
+            }
+            if(clpArrayViewModel.TopArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.TopArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
+            }
+            if(clpArrayViewModel.TopArrowPosition > clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.TopArrowPosition = clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP;
+            }
+            clpArrayViewModel.IsTopAdornerVisible = true;
+            clpArrayViewModel.IsLeftAdornerVisible = false;
+            clpArrayViewModel.IsDefaultAdornerVisible = false;
+            clpArrayViewModel.IsAdornerVisible = true;
         }
 
         private void ArrayLeftHitBox_MouseMove(object sender, MouseEventArgs e)
         {
-            //_divisorHideTimer.Stop();
-            //var clpArrayViewModel = ViewModel as CLPArrayViewModel;
-            //if(clpArrayViewModel == null ||
-            //   !clpArrayViewModel.IsDivisionBehaviorOn ||
-            //   clpArrayViewModel.IsDefaultAdornerVisible)
-            //{
-            //    return;
-            //}
+            _divisorHideTimer.Stop();
+            var clpArrayViewModel = ViewModel as CLPArrayViewModel;
+            if(clpArrayViewModel == null ||
+               !clpArrayViewModel.IsDivisionBehaviorOn ||
+               clpArrayViewModel.IsDefaultAdornerVisible)
+            {
+                return;
+            }
 
-            //ACLPPageBaseViewModel.ClearAdorners(clpArrayViewModel.PageObject.ParentPage);
-            //var clpArray = clpArrayViewModel.PageObject as CLPArray;
-            //if(clpArray != null)
-            //{
-            //    clpArrayViewModel.LeftArrowPosition = e.GetPosition(Array).Y;
-            //}
-            //if(clpArrayViewModel.LeftArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.LeftArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
-            //}
-            //if(clpArrayViewModel.LeftArrowPosition > clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.LeftArrowPosition = clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP;
-            //}
-            //clpArrayViewModel.IsTopAdornerVisible = false;
-            //clpArrayViewModel.IsLeftAdornerVisible = true;
-            //clpArrayViewModel.IsDefaultAdornerVisible = false;
-            //clpArrayViewModel.IsAdornerVisible = true;
+            ACLPPageBaseViewModel.ClearAdorners(clpArrayViewModel.PageObject.ParentPage);
+            var clpArray = clpArrayViewModel.PageObject as CLPArray;
+            if(clpArray != null)
+            {
+                clpArrayViewModel.LeftArrowPosition = e.GetPosition(Array).Y;
+            }
+            if(clpArrayViewModel.LeftArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.LeftArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
+            }
+            if(clpArrayViewModel.LeftArrowPosition > clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.LeftArrowPosition = clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP;
+            }
+            clpArrayViewModel.IsTopAdornerVisible = false;
+            clpArrayViewModel.IsLeftAdornerVisible = true;
+            clpArrayViewModel.IsDefaultAdornerVisible = false;
+            clpArrayViewModel.IsAdornerVisible = true;
         }
 
         private void ArrayDivisionHitBox_MouseLeave(object sender, MouseEventArgs e)
         {
-            //var clpArrayViewModel = ViewModel as CLPArrayViewModel;
-            //if(clpArrayViewModel != null && !clpArrayViewModel.IsDefaultAdornerVisible)
-            //{
-            //    _divisorHideTimer.Start();
-            //}
+            var clpArrayViewModel = ViewModel as CLPArrayViewModel;
+            if(clpArrayViewModel != null && !clpArrayViewModel.IsDefaultAdornerVisible)
+            {
+                _divisorHideTimer.Start();
+            }
         }
 
         private bool _overDivisionAdorner;
@@ -109,66 +109,66 @@ namespace Classroom_Learning_Partner.Views
 
         private void LeftDivisorButton_Mouse(object sender, MouseEventArgs e)
         {
-            //var clpArrayViewModel = ViewModel as CLPArrayViewModel;
-            //if(clpArrayViewModel == null ||
-            //   !clpArrayViewModel.IsDivisionBehaviorOn ||
-            //   clpArrayViewModel.IsDefaultAdornerVisible)
-            //{
-            //    return;
-            //}
-            //var clpArray = clpArrayViewModel.PageObject as CLPArray;
-            //if(clpArray != null)
-            //{
-            //    clpArrayViewModel.LeftArrowPosition = e.GetPosition(Array).Y;
-            //}
-            //if (clpArrayViewModel.LeftArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.LeftArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
-            //}
-            //if (clpArrayViewModel.LeftArrowPosition > clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.LeftArrowPosition = clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP;
-            //}
+            var clpArrayViewModel = ViewModel as CLPArrayViewModel;
+            if(clpArrayViewModel == null ||
+               !clpArrayViewModel.IsDivisionBehaviorOn ||
+               clpArrayViewModel.IsDefaultAdornerVisible)
+            {
+                return;
+            }
+            var clpArray = clpArrayViewModel.PageObject as CLPArray;
+            if(clpArray != null)
+            {
+                clpArrayViewModel.LeftArrowPosition = e.GetPosition(Array).Y;
+            }
+            if (clpArrayViewModel.LeftArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.LeftArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
+            }
+            if (clpArrayViewModel.LeftArrowPosition > clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.LeftArrowPosition = clpArrayViewModel.ArrayHeight - MINIMUM_DIVISION_ADORNER_GAP;
+            }
         }
 
         private void TopDivisorButton_Mouse(object sender, MouseEventArgs e)
         {
-            //var clpArrayViewModel = ViewModel as CLPArrayViewModel;
-            //if(clpArrayViewModel == null ||
-            //   !clpArrayViewModel.IsDivisionBehaviorOn ||
-            //   clpArrayViewModel.IsDefaultAdornerVisible)
-            //{
-            //    return;
-            //}
-            //var clpArray = clpArrayViewModel.PageObject as CLPArray;
-            //if(clpArray == null)
-            //{
-            //    return;
-            //}
-            //clpArrayViewModel.TopArrowPosition = e.GetPosition(Array).X;
-            //if (clpArrayViewModel.TopArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.TopArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
-            //}
-            //if (clpArrayViewModel.TopArrowPosition > clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP)
-            //{
-            //    clpArrayViewModel.TopArrowPosition = clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP;
-            //}
+            var clpArrayViewModel = ViewModel as CLPArrayViewModel;
+            if(clpArrayViewModel == null ||
+               !clpArrayViewModel.IsDivisionBehaviorOn ||
+               clpArrayViewModel.IsDefaultAdornerVisible)
+            {
+                return;
+            }
+            var clpArray = clpArrayViewModel.PageObject as CLPArray;
+            if(clpArray == null)
+            {
+                return;
+            }
+            clpArrayViewModel.TopArrowPosition = e.GetPosition(Array).X;
+            if (clpArrayViewModel.TopArrowPosition < MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.TopArrowPosition = MINIMUM_DIVISION_ADORNER_GAP;
+            }
+            if (clpArrayViewModel.TopArrowPosition > clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP)
+            {
+                clpArrayViewModel.TopArrowPosition = clpArrayViewModel.ArrayWidth - MINIMUM_DIVISION_ADORNER_GAP;
+            }
         }
 
         void _divisorHideTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            //_divisorHideTimer.Stop();
-            //var clpArrayViewModel = ViewModel as CLPArrayViewModel;
-            //if(clpArrayViewModel == null ||
-            //   _overDivisionAdorner ||
-            //   clpArrayViewModel.IsDefaultAdornerVisible)
-            //{
-            //    return;
-            //}
-            //clpArrayViewModel.IsTopAdornerVisible = false;
-            //clpArrayViewModel.IsLeftAdornerVisible = false;
-            //clpArrayViewModel.IsAdornerVisible = false;
+            _divisorHideTimer.Stop();
+            var clpArrayViewModel = ViewModel as CLPArrayViewModel;
+            if(clpArrayViewModel == null ||
+               _overDivisionAdorner ||
+               clpArrayViewModel.IsDefaultAdornerVisible)
+            {
+                return;
+            }
+            clpArrayViewModel.IsTopAdornerVisible = false;
+            clpArrayViewModel.IsLeftAdornerVisible = false;
+            clpArrayViewModel.IsAdornerVisible = false;
         }
     }
 }
