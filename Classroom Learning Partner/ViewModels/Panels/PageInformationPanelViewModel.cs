172521410c7a57a9426ad19fd4c81b4f2184782c
steve <steve>
2014-04-25 19:57:56 -0400

    start of pageinfo panel

---
+++ Classroom Learning Partner/ViewModels/Panels/PageInformationPanelViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CLP.Entities;
+
+namespace Classroom_Learning_Partner.ViewModels
+{
+    public class PageInformationPanelViewModel : APanelBaseViewModel
+    {
+        #region Constructor
+
+        public PageInformationPanelViewModel(CLPPage page)
+        {
+            
+        }
+
+        #endregion //Constructor
+
+        #region Model
+
+         
+
+        #endregion //Model
+         
+    }
+}
