ef2cf332515a68edbf7790ccbd527ca89967753d
steve <steve>
2014-05-31 02:09:22 -0400

    Sort methods in place

--- Classroom Learning Partner/ViewModels/Panels/StagingPanelViewModel.cs
+++ Classroom Learning Partner/ViewModels/Panels/StagingPanelViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using Catel.Data;
 using Catel.MVVM;
@@ -13,8 +10,11 @@ namespace Classroom_Learning_Partner.ViewModels
 {
     public class StagingPanelViewModel : APanelBaseViewModel
     {
-        private static SortDescription _ownerFullNameAscendingSort = new SortDescription("Owner.FullName", ListSortDirection.Ascending);
-        private static SortDescription _submissionTimeDescendingSort = new SortDescription("SubmissionTime", ListSortDirection.Descending);
+        private static readonly SortDescription OwnerFullNameAscendingSort = new SortDescription("Owner.FullName", ListSortDirection.Ascending);
+        private static readonly SortDescription SubmissionTimeAscendingSort = new SortDescription("SubmissionTime", ListSortDirection.Ascending);
+        private static readonly SortDescription SubmissionTimeDescendingSort = new SortDescription("SubmissionTime", ListSortDirection.Descending);
+        private static readonly SortDescription PageNumberAscendingSort = new SortDescription("PageNumber", ListSortDirection.Ascending);
+        private static readonly SortDescription PageNumberDescendingSort = new SortDescription("PageNumber", ListSortDirection.Descending);
 
         public StagingPanelViewModel(Notebook notebook)
         {
@@ -34,40 +34,94 @@ namespace Classroom_Learning_Partner.ViewModels
 
         //observablecollection of PageGroup. PageGroup.GroupName = SubmitterName?
 
+        #region Bindings
+
         /// <summary>
         /// Gets or sets the property value.
         /// </summary>
-        public CollectionViewSource SortedAndFilteredPages
+        public CollectionViewSource SortedAndGroupedPages
         {
-            get { return GetValue<CollectionViewSource>(SortedAndFilteredPagesProperty); }
-            set { SetValue(SortedAndFilteredPagesProperty, value); }
+            get { return GetValue<CollectionViewSource>(SortedAndGroupedPagesProperty); }
+            set { SetValue(SortedAndGroupedPagesProperty, value); }
         }
 
-        public static readonly PropertyData SortedAndFilteredPagesProperty = RegisterProperty("SortedAndFilteredPages", typeof(CollectionViewSource), () => new CollectionViewSource());
+        public static readonly PropertyData SortedAndGroupedPagesProperty = RegisterProperty("SortedAndGroupedPages", typeof(CollectionViewSource), () => new CollectionViewSource());
 
-        public void AddPageToStage(CLPPage page)
-        {
-            
-        }
+        #endregion //Bindings
+
+        #region Commands
 
         /// <summary>
         /// Removes given page from the Staging Panel.
         /// </summary>
         public Command<CLPPage> RemovePageFromStageCommand { get; private set; }
 
-        private void OnRemovePageFromStageCommandExecute(CLPPage page)
-        {
-            //remove from FilteredPages and add to listoftrashed pages Rx will ignore.
-        }
+        private void OnRemovePageFromStageCommandExecute(CLPPage page) { RemovePageFromStage(page); }
 
         /// <summary>
         /// Clears the Stage of all filters, sorts, and contents.
         /// </summary>
         public Command ClearStageCommand { get; private set; }
 
-        private void OnClearStageCommandExecute()
+        private void OnClearStageCommandExecute() { ClearStage(); }
+
+        #endregion //Commands
+
+        #region Methods
+
+        #region Filters
+
+        public void ClearStage()
         {
             
         }
+
+        public void AddPageToStage(CLPPage page)
+        {
+            
+        }
+
+        public void RemovePageFromStage(CLPPage page)
+        {
+            //remove from FilteredPages and add to listoftrashed pages Rx will ignore.
+        }
+
+        public void AppendCollectionOfPagesToStage(ObservableCollection<CLPPage> pages)
+        {
+            
+        }
+
+        #endregion //Filters
+
+        #region Sorts
+
+        public void ApplySortAndGroupByName()
+        {
+            SortedAndGroupedPages.GroupDescriptions.Clear();
+            SortedAndGroupedPages.SortDescriptions.Clear();
+            SortedAndGroupedPages.SortDescriptions.Add(OwnerFullNameAscendingSort);
+            SortedAndGroupedPages.SortDescriptions.Add(PageNumberAscendingSort);
+            SortedAndGroupedPages.SortDescriptions.Add(SubmissionTimeAscendingSort);
+        }
+
+        public void ApplySortAndGroupByPageNumber()
+        {
+            SortedAndGroupedPages.GroupDescriptions.Clear();
+            SortedAndGroupedPages.SortDescriptions.Clear();
+            SortedAndGroupedPages.SortDescriptions.Add(PageNumberAscendingSort);
+            SortedAndGroupedPages.SortDescriptions.Add(OwnerFullNameAscendingSort);
+            SortedAndGroupedPages.SortDescriptions.Add(SubmissionTimeAscendingSort);
+        }
+
+        public void ApplySortByTime()
+        {
+            SortedAndGroupedPages.GroupDescriptions.Clear();
+            SortedAndGroupedPages.SortDescriptions.Clear();
+            SortedAndGroupedPages.SortDescriptions.Add(SubmissionTimeAscendingSort);
+        }
+
+        #endregion //Sorts
+
+        #endregion //Methods
     }
 }
