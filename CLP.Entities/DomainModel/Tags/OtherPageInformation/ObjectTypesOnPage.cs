60a43692b0351be7c18c1c3874ea88bd8418bb3b
stev <stev.code@gmail.com>
2014-08-08 13:38:52 -0400

    ObjectTypesOnPage "Stroke" value renamed to "Ink". "Highlighter" value added.

--- CLP.Entities/DomainModel/Tags/OtherPageInformation/ObjectTypesOnPage.cs
+++ CLP.Entities/DomainModel/Tags/OtherPageInformation/ObjectTypesOnPage.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Windows.Ink;
 using Catel.Data;
 
 namespace CLP.Entities
@@ -12,47 +11,45 @@ namespace CLP.Entities
     {
         #region Constructors
 
-        /// <summary>
-        /// Initializes <see cref="ObjectTypesOnPage" /> from scratch.
-        /// </summary>
+        /// <summary>Initializes <see cref="ObjectTypesOnPage" /> from scratch.</summary>
         public ObjectTypesOnPage() { }
 
-        /// <summary>
-        /// Initializes <see cref="ObjectTypesOnPage" /> from values.
-        /// </summary>
+        /// <summary>Initializes <see cref="ObjectTypesOnPage" /> from values.</summary>
         /// <param name="parentPage">The <see cref="CLPPage" /> the <see cref="ObjectTypesOnPage" /> belongs to.</param>
+        /// <param name="origin"></param>
+        /// <param name="currentUserID"></param>
         public ObjectTypesOnPage(CLPPage parentPage, Origin origin, string currentUserID)
             : base(parentPage, origin)
         {
             IsSingleValueTag = true;
 
-            foreach(var pageObject in parentPage.PageObjects.Where(pageObject => pageObject.OwnerID == parentPage.OwnerID))
+            foreach (var pageObject in parentPage.PageObjects.Where(pageObject => pageObject.OwnerID == parentPage.OwnerID))
             {
-                if(pageObject is FuzzyFactorCard)
+                if (pageObject is FuzzyFactorCard)
                 {
                     ObjectTypes.Add("Division Templates");
                     continue;
                 }
 
-                if(pageObject is CLPArray)
+                if (pageObject is CLPArray)
                 {
                     ObjectTypes.Add("Arrays");
                     continue;
                 }
 
-                if(pageObject is RemainderTiles)
+                if (pageObject is RemainderTiles)
                 {
                     ObjectTypes.Add("Remainder Tiles");
                     continue;
                 }
 
-                if(pageObject is Stamp)
+                if (pageObject is Stamp)
                 {
                     ObjectTypes.Add("Stamps");
                     continue;
                 }
 
-                if(pageObject is Shape)
+                if (pageObject is Shape)
                 {
                     ObjectTypes.Add("Shapes");
                     continue;
@@ -61,16 +58,18 @@ namespace CLP.Entities
 
             ObjectTypes = ObjectTypes.Distinct().ToList();
 
-           // ObjectTypes = parentPage.PageObjects.Where(pageObject => pageObject.OwnerID == parentPage.OwnerID).Select(pageObject => pageObject.GetType().Name).Distinct().ToList();
-            if(parentPage.InkStrokes.Any(stroke => stroke.GetStrokeOwnerID() == parentPage.OwnerID))
+            if (parentPage.InkStrokes.Any(stroke => stroke.GetStrokeOwnerID() == parentPage.OwnerID))
             {
-                ObjectTypes.Add(typeof(Stroke).Name);
+                ObjectTypes.Add("Ink");
+            }
+
+            if (parentPage.InkStrokes.Any(stroke => stroke.GetStrokeOwnerID() == parentPage.OwnerID && stroke.DrawingAttributes.IsHighlighter))
+            {
+                ObjectTypes.Add("Highlighter");
             }
         }
 
-        /// <summary>
-        /// Initializes <see cref="ObjectTypesOnPage" /> based on <see cref="SerializationInfo" />.
-        /// </summary>
+        /// <summary>Initializes <see cref="ObjectTypesOnPage" /> based on <see cref="SerializationInfo" />.</summary>
         /// <param name="info"><see cref="SerializationInfo" /> that contains the information.</param>
         /// <param name="context"><see cref="StreamingContext" />.</param>
         public ObjectTypesOnPage(SerializationInfo info, StreamingContext context)
@@ -80,16 +79,14 @@ namespace CLP.Entities
 
         #region Properties
 
-        /// <summary>
-        /// List of all the <see cref="Type" />s of the relevant <see cref="IPageObject" />s on the <see cref="CLPPage" /> as well as Ink.
-        /// </summary>
+        /// <summary>List of all the <see cref="Type" />s of the relevant <see cref="IPageObject" />s on the <see cref="CLPPage" /> as well as Ink.</summary>
         public List<string> ObjectTypes
         {
             get { return GetValue<List<string>>(ObjectTypesProperty); }
             set { SetValue(ObjectTypesProperty, value); }
         }
 
-        public static readonly PropertyData ObjectTypesProperty = RegisterProperty("ObjectTypes", typeof(List<string>), () => new List<string>());
+        public static readonly PropertyData ObjectTypesProperty = RegisterProperty("ObjectTypes", typeof (List<string>), () => new List<string>());
 
         #region ATagBase Overrides
 
