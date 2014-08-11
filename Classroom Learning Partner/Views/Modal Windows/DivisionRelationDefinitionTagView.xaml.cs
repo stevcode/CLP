7bf048cf3123f1ae35504c976854f3dd6bef6eee
stev <stev.code@gmail.com>
2014-08-11 17:34:59 -0400

    Working DivisionAnswerDefinition file commit.

---
+++ Classroom Learning Partner/Views/Modal Windows/DivisionRelationDefinitionTagView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Catel.Windows;
+using Classroom_Learning_Partner.ViewModels;
+
+namespace Classroom_Learning_Partner.Views.Modal_Windows
+{
+    /// <summary>Interaction logic for MultiplicationRelationDefinitionTagView.xaml.</summary>
+    public partial class DivisionRelationDefinitionTagView : DataWindow
+    {
+        /// <summary>Initializes a new instance of the <see cref="MultiplicationRelationDefinitionTagView" /> class.</summary>
+        public DivisionRelationDefinitionTagView(DivisionRelationDefinitionTagViewModel viewModel)
+            : base(viewModel) { InitializeComponent(); }
+    }
+}
