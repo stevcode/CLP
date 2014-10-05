41fa6e35f6f5a3e7f04b1deb3cff2ca7944d0f01
stev <stev.code@gmail.com>
2014-10-05 02:31:07 -0400

    CustomControl for new RibbonButtons

---
+++ Classroom Learning Partner/Views/Ribbon/MajorRibbonView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Classroom_Learning_Partner.ViewModels;
+
+namespace Classroom_Learning_Partner.Views
+{
+    /// <summary>Interaction logic for MajorRibbonView.xaml</summary>
+    public partial class MajorRibbonView
+    {
+        public MajorRibbonView() { InitializeComponent(); }
+
+        protected override Type GetViewModelType() { return typeof(RibbonViewModel); }
+    }
+}
