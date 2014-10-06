0df858d82df7cb61bc7ae85cc6d4b968188398cf
Emily Benz <emilycbenz@gmail.com>
2014-10-06 10:58:06 -0400

    NumberLine model update

--- CLP.Entities/DomainModel/PageObjects/Arrays/NumberLine.cs
+++ CLP.Entities/DomainModel/PageObjects/Arrays/NumberLine.cs
@@ -1,9 +1,65 @@
 ﻿using System;
+using System;
+using System.Collections.ObjectModel;
 using System.Runtime.Serialization;
 using Catel.Data;
 
 namespace CLP.Entities
 {
+    [Serializable]
+    public class NumberLineTick : AEntityBase
+    {
+
+        #region Constructors
+
+        public NumberLineTick() { }
+
+        public NumberLineTick(bool isNumberVisible) { IsNumberVisible = isNumberVisible; }
+
+        public NumberLineTick(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+
+        #endregion //Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Is the tick marks number visible
+        /// </summary>
+        public bool IsNumberVisible
+        {
+            get { return GetValue<bool>(IsNumberVisibleProperty); }
+            set { SetValue(IsNumberVisibleProperty, value); }
+        }
+
+        public static readonly PropertyData IsNumberVisibleProperty = RegisterProperty("IsNumberVisible", typeof (bool), false);
+
+        /// <summary>
+        /// Is the tick visible on the number line
+        /// </summary>
+        public bool IsTickVisible
+        {
+            get { return GetValue<bool>(IsTickVisibleProperty); }
+            set { SetValue(IsTickVisibleProperty, value); }
+        }
+
+        public static readonly PropertyData IsTickVisibleProperty = RegisterProperty("IsTickVisible", typeof (bool), true);
+
+        /// <summary>
+        /// Has the user marked the tick mark
+        /// </summary>
+        public bool IsMarked
+        {
+            get { return GetValue<bool>(IsMarkedProperty); }
+            set { SetValue(IsMarkedProperty, value); }
+        }
+
+        public static readonly PropertyData IsMarkedProperty = RegisterProperty("IsMarked", typeof (bool), false);
+
+        #endregion //Properties
+    }
+
     [Serializable]
     public class NumberLine : APageObjectBase
     {
@@ -14,7 +70,7 @@ namespace CLP.Entities
         public NumberLine(CLPPage parentPage, int numberLength)
             : base(parentPage)
         {
-            NumberLineLength = numberLength;
+            NumberLineSize = numberLength;
             Height = 60;
             Width = 800;
         }
@@ -26,7 +82,20 @@ namespace CLP.Entities
 
         #region Properties
 
-        private const double MIN_NUMBERLINE_LENGTH = 25.0;
+        public double ArrowLength
+        {
+            get { return 40.0; }
+        }
+
+        public double NumberLineLength
+        {
+            get { return Width - ArrowLength * 2; }
+        }
+
+        public double TickLength
+        {
+            get { return NumberLineLength / NumberLineSize; }
+        }
 
         public override bool IsBackgroundInteractable
         {
@@ -34,17 +103,69 @@ namespace CLP.Entities
         }
 
         /// <summary>Length of number line</summary>
-        public int NumberLineLength
+        public int NumberLineSize
         {
-            get { return GetValue<int>(NumberLineLengthProperty); }
-            set { SetValue(NumberLineLengthProperty, value); }
+            get { return GetValue<int>(NumberLineSizeProperty); }
+            set { SetValue(NumberLineSizeProperty, value); }
         }
 
-        public static readonly PropertyData NumberLineLengthProperty = RegisterProperty("NumberLineLength", typeof (int), 0);
+        public static readonly PropertyData NumberLineSizeProperty = RegisterProperty("NumberLineSize", typeof (int), 0, OnNumberLineSizeChanged);
+
+        private static void OnNumberLineSizeChanged(object sender, AdvancedPropertyChangedEventArgs advancedPropertyChangedEventArgs)
+        {
+            var numberLine = sender as NumberLine;
+            if (numberLine == null)
+            {
+                return;
+            }
+            numberLine.CreateTicks();
+        }
+
+        /// <summary>
+        /// A collection of the ticks of the number line
+        /// </summary>
+        public ObservableCollection<NumberLineTick> Ticks
+        {
+            get { return GetValue<ObservableCollection<NumberLineTick>>(TicksProperty); }
+            set { SetValue(TicksProperty, value); }
+        }
+
+        public static readonly PropertyData TicksProperty = RegisterProperty("Ticks", typeof (ObservableCollection<NumberLineTick>), () => new ObservableCollection<NumberLineTick>());
 
         #endregion //Properties
 
         #region Methods
+
+        public void CreateTicks()
+        {
+            Ticks.Clear();
+            int defaultInteger = NumberLineSize <= 10 ? 1 : 5;
+            for (int i = 0; i <= NumberLineSize; i++)
+            {
+                bool labelVisible = false;
+                if (i==0 || i==NumberLineSize)
+                {
+                    labelVisible = true;
+                }
+                else if (i % defaultInteger == 0)
+                {
+                    labelVisible = true;
+                }
+                Ticks.Add(new NumberLineTick(labelVisible));
+            }
+
+        }
+
+        protected override void OnPropertyChanged(AdvancedPropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Width")
+            {
+                RaisePropertyChanged("TickLength");
+                RaisePropertyChanged("NumberLineLength");
+            }
+            base.OnPropertyChanged(e);
+        }
+
 
         public override IPageObject Duplicate()
         {
