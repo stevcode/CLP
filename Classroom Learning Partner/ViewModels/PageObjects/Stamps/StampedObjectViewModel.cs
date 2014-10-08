097e6c4f84d90514cb5d6a9fef0acd2536336657
stev <stev.code@gmail.com>
2014-10-08 07:14:52 -0400

    Stampedobjects fixed to work with Piles

--- Classroom Learning Partner/ViewModels/PageObjects/Stamps/StampedObjectViewModel.cs
+++ Classroom Learning Partner/ViewModels/PageObjects/Stamps/StampedObjectViewModel.cs
@@ -1,28 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Windows;
 using System.Windows.Media;
 using Catel.Data;
 using Catel.MVVM;
+using Classroom_Learning_Partner.Views.Modal_Windows;
 using CLP.Entities;
 
 namespace Classroom_Learning_Partner.ViewModels
 {
-    /// <summary>
-    /// UserControl view model.
-    /// </summary>
+    /// <summary>UserControl view model.</summary>
     public class StampedObjectViewModel : APageObjectBaseViewModel
     {
         #region Constructor
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="StampedObjectViewModel" /> class.
-        /// </summary>
+        /// <summary>Initializes a new instance of the <see cref="StampedObjectViewModel" /> class.</summary>
         public StampedObjectViewModel(StampedObject stampedObject)
         {
             PageObject = stampedObject;
-            if(App.MainWindowViewModel.ImagePool.ContainsKey(stampedObject.ImageHashID))
+            RaisePropertyChanged("IsGroupStampedObject");
+            if (App.MainWindowViewModel.ImagePool.ContainsKey(stampedObject.ImageHashID))
             {
                 SourceImage = App.MainWindowViewModel.ImagePool[stampedObject.ImageHashID];
             }
@@ -30,32 +30,35 @@ namespace Classroom_Learning_Partner.ViewModels
             {
                 var filePath = string.Empty;
                 var imageFilePaths = Directory.EnumerateFiles(MainWindowViewModel.ImageCacheDirectory);
-                foreach(var imageFilePath in from imageFilePath in imageFilePaths
-                                             let imageHashID = Path.GetFileNameWithoutExtension(imageFilePath)
-                                             where imageHashID == stampedObject.ImageHashID
-                                             select imageFilePath)
+                foreach (var imageFilePath in from imageFilePath in imageFilePaths
+                                              let imageHashID = Path.GetFileNameWithoutExtension(imageFilePath)
+                                              where imageHashID == stampedObject.ImageHashID
+                                              select imageFilePath)
                 {
                     filePath = imageFilePath;
                     break;
                 }
 
                 var bitmapImage = CLPImage.GetImageFromPath(filePath);
-                if(bitmapImage != null)
+                if (bitmapImage != null)
                 {
                     SourceImage = bitmapImage;
                     App.MainWindowViewModel.ImagePool.Add(stampedObject.ImageHashID, bitmapImage);
                 }
             }
 
-            foreach(var strokePath in stampedObject.SerializedStrokes.Select(serializedStroke => serializedStroke.ToStroke()).Select(stroke => new StrokePathDTO(stroke))) 
+            foreach (
+                var strokePath in
+                    stampedObject.SerializedStrokes.Select(serializedStroke => serializedStroke.ToStroke())
+                                 .Select(stroke => new StrokePathDTO(stroke)))
             {
                 StrokePaths.Add(strokePath);
             }
+
+            ParameterizeStampedObjectCommand = new Command(OnParameterizeStampedObjectCommandExecute);
         }
 
-        /// <summary>
-        /// Gets the title of the view model.
-        /// </summary>
+        /// <summary>Gets the title of the view model.</summary>
         /// <value>The title.</value>
         public override string Title
         {
@@ -66,23 +69,17 @@ namespace Classroom_Learning_Partner.ViewModels
 
         #region Model
 
-        /// <summary>
-        /// Whether or not the StampCopy is a copy of a Collection Stamp.
-        /// This property is automatically mapped to the corresponding property in PageObject.
-        /// </summary>
+        /// <summary>Type of <see cref="StampedObject" />.</summary>
         [ViewModelToModel("PageObject")]
-        public bool IsStampedCollection
+        public StampedObjectTypes StampedObjectType
         {
-            get { return GetValue<bool>(IsStampedCollectionProperty); }
-            set { SetValue(IsStampedCollectionProperty, value); }
+            get { return GetValue<StampedObjectTypes>(StampedObjectTypeProperty); }
+            set { SetValue(StampedObjectTypeProperty, value); }
         }
 
-        public static readonly PropertyData IsStampedCollectionProperty = RegisterProperty("IsStampedCollection", typeof(bool));
+        public static readonly PropertyData StampedObjectTypeProperty = RegisterProperty("StampedObjectType", typeof (StampedObjectTypes));
 
-        /// <summary>
-        /// Number of parts represented by the StampCopy. Only visible for collection copies.
-        /// This property is automatically mapped to the corresponding property in PageObject.
-        /// </summary>
+        /// <summary>Number of parts represented by the StampCopy. Only visible for collection copies.</summary>
         [ViewModelToModel("PageObject")]
         public int Parts
         {
@@ -90,34 +87,116 @@ namespace Classroom_Learning_Partner.ViewModels
             set { SetValue(PartsProperty, value); }
         }
 
-        public static readonly PropertyData PartsProperty = RegisterProperty("Parts", typeof(int));
+        public static readonly PropertyData PartsProperty = RegisterProperty("Parts", typeof (int));
 
         #endregion //Model
 
         #region Binding
 
-        /// <summary>
-        /// The visible image, loaded from the page's ImagePool.
-        /// </summary>
+        public bool IsGroupStampedObject
+        {
+            get
+            {
+                var stampedObject = PageObject as StampedObject;
+                if (stampedObject == null)
+                {
+                    return false;
+                }
+
+                return StampedObjectType == StampedObjectTypes.GroupStampedObject || StampedObjectType == StampedObjectTypes.EmptyGroupStampedObject;
+            }
+        }
+
+        /// <summary>The visible image, loaded from the page's ImagePool.</summary>
         public ImageSource SourceImage
         {
             get { return GetValue<ImageSource>(SourceImageProperty); }
             set { SetValue(SourceImageProperty, value); }
         }
 
-        public static readonly PropertyData SourceImageProperty = RegisterProperty("SourceImage", typeof(ImageSource));
+        public static readonly PropertyData SourceImageProperty = RegisterProperty("SourceImage", typeof (ImageSource));
 
-        /// <summary>
-        /// List of <see cref="StrokePathDTO" />s that make up the <see cref="StampedObject" />.
-        /// </summary>
+        /// <summary>List of <see cref="StrokePathDTO" />s that make up the <see cref="StampedObject" />.</summary>
         public ObservableCollection<StrokePathDTO> StrokePaths
         {
             get { return GetValue<ObservableCollection<StrokePathDTO>>(StrokePathsProperty); }
             set { SetValue(StrokePathsProperty, value); }
         }
 
-        public static readonly PropertyData StrokePathsProperty = RegisterProperty("StrokePaths", typeof(ObservableCollection<StrokePathDTO>), () => new ObservableCollection<StrokePathDTO>());
+        public static readonly PropertyData StrokePathsProperty = RegisterProperty("StrokePaths",
+                                                                                   typeof (ObservableCollection<StrokePathDTO>),
+                                                                                   () => new ObservableCollection<StrokePathDTO>());
 
         #endregion //Binding
+
+        #region Commands
+
+        /// <summary>Parameterizes the StampedObject.</summary>
+        public Command ParameterizeStampedObjectCommand { get; private set; }
+
+        private void OnParameterizeStampedObjectCommandExecute()
+        {
+            var stampedObject = PageObject as StampedObject;
+            if (stampedObject == null)
+            {
+                return;
+            }
+
+            var keyPad = new KeypadWindowView("How many copies?", 21)
+            {
+                Owner = Application.Current.MainWindow,
+                WindowStartupLocation = WindowStartupLocation.Manual,
+                Top = 100,
+                Left = 100
+            };
+            keyPad.ShowDialog();
+            if (keyPad.DialogResult != true ||
+                keyPad.NumbersEntered.Text.Length <= 0)
+            {
+                return;
+            }
+
+            var numberOfCopies = Int32.Parse(keyPad.NumbersEntered.Text);
+
+            var initialXPosition = XPosition + Width + 10.0;
+            var initialYPosition = YPosition;
+
+            var stampCopiesToAdd = new List<IPageObject>();
+            for (var i = 0; i < numberOfCopies; i++)
+            {
+                var newStampedObject = stampedObject.Duplicate() as StampedObject;
+
+                if (newStampedObject == null)
+                {
+                    continue;
+                }
+
+                newStampedObject.XPosition = initialXPosition;
+                newStampedObject.YPosition = initialYPosition;
+
+                stampCopiesToAdd.Add(stampedObject);
+                if (initialXPosition + 2 * stampedObject.Width + 5 < PageObject.ParentPage.Width)
+                {
+                    initialXPosition += stampedObject.Width + 5;
+                }
+                else if (initialYPosition + 2 * stampedObject.Height + 5 < PageObject.ParentPage.Height)
+                {
+                    initialXPosition = 25;
+                    initialYPosition += stampedObject.Height + 5;
+                }
+
+                foreach (var pageObject in stampedObject.AcceptedPageObjects)
+                {
+                    var newPageObject = pageObject.Duplicate();
+                    newPageObject.XPosition = stampedObject.XPosition + (pageObject.XPosition - stampedObject.XPosition);
+                    newPageObject.YPosition = stampedObject.YPosition + (pageObject.YPosition - stampedObject.YPosition);
+                    stampCopiesToAdd.Add(newPageObject);
+                }
+            }
+
+            ACLPPageBaseViewModel.AddPageObjectsToPage(stampedObject.ParentPage, stampCopiesToAdd);
+        }
+
+        #endregion //Commands
     }
 }
