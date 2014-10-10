0b6ed3be29acc5c259d15eafc4cae451bf77d5b4
Emily Benz <emilycbenz@gmail.com>
2014-10-10 11:31:24 -0400

    readded GUID extension methods

---
+++ CLP.Entities/DomainModel/Utility/Extentions/GuidExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Catel;
+
+namespace CLP.Entities
+{
+    public static class GuidExtensions
+    {
+        public static string ToCompactID(this Guid guid)
+        {
+            Argument.IsNotNull("guid", guid);
+
+            var compactedID = Convert.ToBase64String(guid.ToByteArray());
+            compactedID = compactedID.Replace("/", "_").Replace("+", "-").Replace("=","");
+  
+            return compactedID;
+        }
+    }
+}
