0679660c0bbab5de894e2d56cce7f8203113aeb6
stev <stev.code@gmail.com>
2014-10-13 06:56:28 -0400

    Start of RibbonButtons, GroupedRibbonButton created, 2 line text boxes on buttons

---
+++ CLP.CustomControls/GroupedRibbonButton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CLP.CustomControls
+{
+    /// <summary></summary>
+    public class GroupedRibbonButton : RadioButton
+    {
+        static GroupedRibbonButton()
+        {
+            // Initialize as a lookless control.
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (RibbonButton), new FrameworkPropertyMetadata(typeof (GroupedRibbonButton)));
+        }
+
+        public GroupedRibbonButton(string text, string groupName, string packUri, ICommand command)
+        {
+            Text = text;
+            GroupName = groupName;
+            LargeImageSource = new ImageSourceConverter().ConvertFromString(packUri) as ImageSource;
+            Command = command;
+
+            var dict = new ResourceDictionary();
+            var uri = new Uri(@"pack://application:,,,/Resources/CLPBrushes.xaml");
+            dict.Source = uri;
+            var color = dict["MainColor"] as Brush;
+            Background = color;
+        }
+
+        #region Dependency Properties
+
+        /// <summary>24x24 pixel image for the button.</summary>
+        public ImageSource LargeImageSource
+        {
+            get { return (ImageSource)GetValue(LargeImageSourceProperty); }
+            set { SetValue(LargeImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty LargeImageSourceProperty = DependencyProperty.Register("LargeImageSource",
+                                                                                                         typeof (ImageSource),
+                                                                                                         typeof (GroupedRibbonButton),
+                                                                                                         new UIPropertyMetadata(null));
+
+        /// <summary>Text for the button.</summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text",
+                                                                                             typeof (string),
+                                                                                             typeof (GroupedRibbonButton),
+                                                                                             new UIPropertyMetadata(null));
+
+        #endregion //Dependency Properties
+    }
+}
--- Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
+++ Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 using Catel.Data;
 using Catel.MVVM;
 using CLP.CustomControls;
 using CLP.Entities;
+using Shape = CLP.Entities.Shape;
 
 namespace Classroom_Learning_Partner.ViewModels
 {
@@ -25,6 +26,7 @@ namespace Classroom_Learning_Partner.ViewModels
         public MajorRibbonViewModel()
         {
             InitializeCommands();
+            InitializeButtons();
             SetRibbonButtons();
         }
 
@@ -34,39 +36,101 @@ namespace Classroom_Learning_Partner.ViewModels
             InsertCircleCommand = new Command(OnInsertCircleCommandExecute);
         }
 
+        private void InitializeButtons()
+        {
+            var dict = new ResourceDictionary();
+            var uri = new Uri(@"pack://application:,,,/Resources/CLPBrushes.xaml");
+            dict.Source = uri;
+            var grayEdgeColor = dict["GrayEdgeColor"] as Brush;
+
+            _separater = new Line
+                         {
+                             Y1 = 0,
+                             Y2 = 1,
+                             Stretch = Stretch.Fill,
+                             Margin = new Thickness(2),
+                             Stroke = grayEdgeColor,
+                             StrokeThickness = 1
+                         };
+
+            //Shapes
+            //TODO: Better Icons
+            _insertSquareButton = new RibbonButton("Insert Square", "pack://application:,,,/Images/AddSquare.png", InsertCircleCommand);
+            _insertCircleButton = new RibbonButton("Insert Circle", "pack://application:,,,/Images/AddCircle.png", InsertCircleCommand);
+            _insertHorizontalLineButton = new RibbonButton("Insert Horizontal Line",
+                                                           "pack://application:,,,/Images/HorizontalLineIcon.png",
+                                                           InsertCircleCommand);
+            _insertVerticalLineButton = new RibbonButton("Insert Vertical Line",
+                                                         "pack://application:,,,/Images/VerticalLineIcon.png",
+                                                         InsertCircleCommand);
+            _insertProtractorButton = new RibbonButton("Insert Protractor", "pack://application:,,,/Images/Protractor64.png", InsertCircleCommand);
+        }
+
+        #region Buttons
+
+        private Line _separater;
+
+        #region Insert PageObject Buttons
+
+        //Images
+        private RibbonButton _insertImageButton;
+
+        //Stamps
+        private RibbonButton _insertStampButton;
+        private RibbonButton _insertImageStampButton;
+        private RibbonButton _insertGroupStampButton;
+        private RibbonButton _insertImageGroupStampButton;
+        private RibbonButton _insertPileButton;
+
+        //Arrays
+        private RibbonButton _insertArrayButton;
+        private RibbonButton _insert10x10ArrayButton;
+        private RibbonButton _insertArrayCardButton;
+        private RibbonButton _insertFactorCardButton;
+
+        //Division Templates
+        private RibbonButton _insertDivisionTemplateButton;
+        private RibbonButton _insertDivisionTemplateWithTilesButton;
+
+        //Text
+        private RibbonButton _insertTextBoxButton;
+
+        //Shapes
+        private RibbonButton _insertSquareButton;
+        private RibbonButton _insertCircleButton;
+        private RibbonButton _insertHorizontalLineButton;
+        private RibbonButton _insertVerticalLineButton;
+        private RibbonButton _insertProtractorButton;
+
+        #endregion //Insert PageObject Buttons
+
+        #endregion //Buttons
+
         #region Bindings
 
-        /// <summary>
-        /// List of the buttons currently on the Ribbon.
-        /// </summary>
-        public ObservableCollection<Button> Buttons
+        /// <summary>List of the buttons currently on the Ribbon.</summary>
+        public ObservableCollection<UIElement> Buttons
         {
-            get { return GetValue<ObservableCollection<Button>>(ButtonsProperty); }
+            get { return GetValue<ObservableCollection<UIElement>>(ButtonsProperty); }
             set { SetValue(ButtonsProperty, value); }
         }
 
-        public static readonly PropertyData ButtonsProperty = RegisterProperty("Buttons", typeof (ObservableCollection<Button>), () => new ObservableCollection<Button>()); 
+        public static readonly PropertyData ButtonsProperty = RegisterProperty("Buttons",
+                                                                               typeof (ObservableCollection<UIElement>),
+                                                                               () => new ObservableCollection<UIElement>());
 
         #endregion //Bindings
-        
 
         #region Commands
 
-        /// <summary>
-        /// Brings up the BackStage.
-        /// </summary>
+        /// <summary>Brings up the BackStage.</summary>
         public Command ShowBackStageCommand { get; private set; }
 
-        private void OnShowBackStageCommandExecute()
-        {
-
-        }
+        private void OnShowBackStageCommandExecute() { }
 
         #region Insert PageObject Commands
 
-        /// <summary>
-        /// Inserts a circle.
-        /// </summary>
+        /// <summary>Inserts a circle.</summary>
         public Command InsertCircleCommand { get; private set; }
 
         private void OnInsertCircleCommandExecute()
@@ -83,9 +147,13 @@ namespace Classroom_Learning_Partner.ViewModels
 
         public void SetRibbonButtons()
         {
-            var insertShapeButton = new RibbonButton("Insert Circle", "pack://application:,,,/Resources/Images/Pile32.png", InsertCircleCommand);
-            
-            Buttons.Add(insertShapeButton);
+            Buttons.Add(_separater);
+
+            Buttons.Add(_insertSquareButton);
+            Buttons.Add(_insertCircleButton);
+            Buttons.Add(_insertHorizontalLineButton);
+            Buttons.Add(_insertVerticalLineButton);
+            Buttons.Add(_insertProtractorButton);
         }
 
         #endregion //Methods
