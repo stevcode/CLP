509d690977fca89ab994f8e1a1a7cf85b0c4c604
stev <stev.code@gmail.com>
2014-11-18 23:11:41 -0500

    increased pageObject tap tolerance to make selecting a pageObject easier.

--- Classroom Learning Partner/ViewModels/PageObjects/APageObjectBaseViewModel.cs
+++ Classroom Learning Partner/ViewModels/PageObjects/APageObjectBaseViewModel.cs
@@ -269,7 +269,7 @@ namespace Classroom_Learning_Partner.ViewModels
 
             var deltaX = Math.Abs(startingPoint.X - XPosition);
             var deltaY = Math.Abs(startingPoint.Y - YPosition);
-            var wasDraggedTolerance = App.MainWindowViewModel.CurrentProgramMode == ProgramModes.Projector ? 5.0 : 1.0;
+            var wasDraggedTolerance = 10.0; // App.MainWindowViewModel.CurrentProgramMode == ProgramModes.Projector ? 5.0 : 1.0;
             var wasDragged = Math.Max(deltaX, deltaY) > wasDraggedTolerance;
 
             if (wasDragged)
