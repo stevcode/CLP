b4ad44f800acd6632c30caa6ad52ab2f900d3fbe
stev <stev.code@gmail.com>
2014-12-07 18:13:41 -0500

    preliminary visuals for animation control

---
+++ Classroom Learning Partner/ViewModels/Ribbon/AnimationControlRibbonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Catel.MVVM;
+
+namespace Classroom_Learning_Partner.ViewModels
+{
+    public class AnimationControlRibbonViewModel : ViewModelBase
+    {
+        public AnimationControlRibbonViewModel()
+        {
+
+        }
+    }
+}
