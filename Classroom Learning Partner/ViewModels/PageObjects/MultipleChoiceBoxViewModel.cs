8e320515272ed400ac72c23c19ed6c32b0d6bb81
stev <stev.code@gmail.com>
2014-12-14 22:50:14 -0500

    multiple choice box viewModel completed

--- Classroom Learning Partner/ViewModels/PageObjects/MultipleChoiceBoxViewModel.cs
+++ Classroom Learning Partner/ViewModels/PageObjects/MultipleChoiceBoxViewModel.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Windows.Documents;
+using System.Linq;
 using CLP.Entities;
 
 namespace Classroom_Learning_Partner.ViewModels
@@ -8,10 +8,7 @@ namespace Classroom_Learning_Partner.ViewModels
     {
         #region Constructor
 
-        public MultipleChoiceBoxViewModel(MultipleChoiceBox multipleChoiceBox)
-        {
-            PageObject = multipleChoiceBox;
-        } 
+        public MultipleChoiceBoxViewModel(MultipleChoiceBox multipleChoiceBox) { PageObject = multipleChoiceBox; }
 
         #endregion //Constructor
 
@@ -21,10 +18,36 @@ namespace Classroom_Learning_Partner.ViewModels
         {
             get
             {
-                return new List<string>();
+                var choiceLabels = new List<string>();
+                var multipleChoiceBox = PageObject as MultipleChoiceBox;
+                if (multipleChoiceBox == null)
+                {
+                    return choiceLabels;
+                }
+
+                choiceLabels.AddRange(
+                                      Enumerable.Range(1, multipleChoiceBox.NumberOfChoices)
+                                                .Select(i => multipleChoiceBox.LabelType == MultipleChoiceLabelTypes.Numbers ? i.ToString() : ToUpperLetter(i)));
+
+                return choiceLabels;
             }
         }
 
         #endregion //Bindings
+
+        #region Methods
+
+        private string ToUpperLetter(int index)
+        {
+            if (index < 1 ||
+                index > 26)
+            {
+                return "aa";
+            }
+
+            return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[index - 1].ToString();
+        }
+
+        #endregion //Methods
     }
 }
---
+++ Classroom Learning Partner/Views/PageObjects/MultipleChoiceBoxView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Classroom_Learning_Partner.ViewModels;
+
+namespace Classroom_Learning_Partner.Views
+{
+    /// <summary>Interaction logic for MultipleChoiceBoxView.xaml</summary>
+    public partial class MultipleChoiceBoxView
+    {
+        public MultipleChoiceBoxView() { InitializeComponent(); }
+
+        protected override Type GetViewModelType() { return typeof (MultipleChoiceBoxViewModel); }
+    }
+}
