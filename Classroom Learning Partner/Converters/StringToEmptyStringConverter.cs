57d5b2736a46fe6d9097ad1ff31444c0fcf2bf55
stev <stev.code@gmail.com>
2015-04-23 13:51:52 -0400

    Locking of Tag delete button and conversion from FullName to DisplayName where appropriate.

---
+++ Classroom Learning Partner/Converters/StringToEmptyStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Classroom_Learning_Partner.Converters
+{
+    public class StringToEmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = value as string;
+            var stringToMatch = parameter as string;
+
+            if (val == null ||
+                stringToMatch == null ||
+                val == stringToMatch)
+            {
+                return string.Empty;
+            }
+
+            return val;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
