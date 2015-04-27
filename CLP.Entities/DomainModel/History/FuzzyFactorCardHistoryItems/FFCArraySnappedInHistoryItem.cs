09efc88b6c74129a6feae9b9b2e79263f1c01485
stev <stev.code@gmail.com>
2015-04-27 03:41:10 -0400

    FFCArraySnappedInHistoryItem verified

--- CLP.Entities/DomainModel/History/FuzzyFactorCardHistoryItems/FFCArraySnappedInHistoryItem.cs
+++ CLP.Entities/DomainModel/History/FuzzyFactorCardHistoryItems/FFCArraySnappedInHistoryItem.cs
@@ -9,27 +9,21 @@ namespace CLP.Entities
     {
         #region Constructors
 
-        /// <summary>
-        /// Initializes <see cref="FFCArraySnappedInHistoryItem" /> from scratch.
-        /// </summary>
+        /// <summary>Initializes <see cref="FFCArraySnappedInHistoryItem" /> from scratch.</summary>
         public FFCArraySnappedInHistoryItem() { }
 
-        /// <summary>
-        /// Initializes <see cref="FFCArraySnappedInHistoryItem" /> with a parent <see cref="CLPPage" />.
-        /// </summary>
+        /// <summary>Initializes <see cref="FFCArraySnappedInHistoryItem" /> with a parent <see cref="CLPPage" />.</summary>
         /// <param name="parentPage">The <see cref="CLPPage" /> the <see cref="IHistoryItem" /> is part of.</param>
         /// <param name="owner">The <see cref="Person" /> who created the <see cref="IHistoryItem" />.</param>
-        public FFCArraySnappedInHistoryItem(CLPPage parentPage, Person owner, string fuzzyFactorCardID, CLPArray snappedInArray)
+        public FFCArraySnappedInHistoryItem(CLPPage parentPage, Person owner, string divisionTemplateID, CLPArray snappedInArray)
             : base(parentPage, owner)
         {
-            FuzzyFactorCardID = fuzzyFactorCardID;
+            FuzzyFactorCardID = divisionTemplateID;
             SnappedInArrayID = snappedInArray.ID;
             parentPage.History.TrashedPageObjects.Add(snappedInArray);
         }
 
-        /// <summary>
-        /// Initializes a new object based on <see cref="SerializationInfo" />.
-        /// </summary>
+        /// <summary>Initializes a new object based on <see cref="SerializationInfo" />.</summary>
         /// <param name="info"><see cref="SerializationInfo" /> that contains the information.</param>
         /// <param name="context"><see cref="StreamingContext" />.</param>
         protected FFCArraySnappedInHistoryItem(SerializationInfo info, StreamingContext context)
@@ -44,92 +38,105 @@ namespace CLP.Entities
             get { return 600; }
         }
 
-        /// <summary>
-        /// UniqueID of the FFC which had an array snapped inside
-        /// </summary>
+        /// <summary>UniqueID of the FFC which had an array snapped inside</summary>
         public string FuzzyFactorCardID
         {
             get { return GetValue<string>(FuzzyFactorCardIDProperty); }
             set { SetValue(FuzzyFactorCardIDProperty, value); }
         }
 
-        public static readonly PropertyData FuzzyFactorCardIDProperty = RegisterProperty("FuzzyFactorCardID", typeof(string), string.Empty);
+        public static readonly PropertyData FuzzyFactorCardIDProperty = RegisterProperty("FuzzyFactorCardID", typeof (string), string.Empty);
 
-        /// <summary>
-        /// UniqueID of the array that wass snapped in and then deleted.
-        /// </summary>
+        /// <summary>UniqueID of the array that wass snapped in and then deleted.</summary>
         public string SnappedInArrayID
         {
             get { return GetValue<string>(SnappedInArrayIDProperty); }
             set { SetValue(SnappedInArrayIDProperty, value); }
         }
 
-        public static readonly PropertyData SnappedInArrayIDProperty = RegisterProperty("SnappedInArrayID", typeof(string), string.Empty);
+        public static readonly PropertyData SnappedInArrayIDProperty = RegisterProperty("SnappedInArrayID", typeof (string), string.Empty);
 
         public override string FormattedValue
         {
             get
             {
-                var snappedArray = ParentPage.GetPageObjectByIDOnPageOrInHistory(SnappedInArrayID) as CLPArray;
-                var ffc = ParentPage.GetPageObjectByIDOnPageOrInHistory(FuzzyFactorCardID) as FuzzyFactorCard;
-                string formattedValue = string.Format("Index # {0}, Snapped array({1} by {2}) into fuzzy factor card ({3} by {4}).", 
-                    HistoryIndex, snappedArray.Rows, snappedArray.Columns, ffc.Rows, ffc.Columns);
-                return formattedValue;
+                var divisionTemplate = ParentPage.GetPageObjectByIDOnPageOrInHistory(FuzzyFactorCardID) as FuzzyFactorCard;
+                if (divisionTemplate == null)
+                {
+                    return string.Format("[ERROR] on Index #{0}, Division Template for Array Snapped In not found on page or in history.", HistoryIndex);
+                }
+
+                var array = ParentPage.GetPageObjectByIDOnPageOrInHistory(SnappedInArrayID) as CLPArray;
+                if (array == null)
+                {
+                    return string.Format("[ERROR] on Index #{0}, Array for Array Snapped In not found on page or in history.", HistoryIndex);
+                }
+
+                return string.Format("Index #{0}, Snapped Array [{1}x{2}] into Division Template [{3}/{4}].",
+                                     HistoryIndex,
+                                     array.Rows,
+                                     array.Columns,
+                                     divisionTemplate.Dividend,
+                                     divisionTemplate.Rows);
             }
         }
-        
+
         #endregion //Properties
 
         #region Methods
 
-        /// <summary>
-        /// Method that will actually undo the action. Already incorporates error checking for existance of ParentPage.
-        /// </summary>
+        /// <summary>Method that will actually undo the action. Already incorporates error checking for existance of ParentPage.</summary>
         protected override void UndoAction(bool isAnimationUndo)
         {
-            var fuzzyFactorCard = ParentPage.GetVerifiedPageObjectOnPageByID(FuzzyFactorCardID) as FuzzyFactorCard;
-            var snappedInArray = ParentPage.GetVerifiedPageObjectInTrashByID(SnappedInArrayID) as CLPArray;
-            if(fuzzyFactorCard == null ||
-               snappedInArray == null)
+            var divisionTemplate = ParentPage.GetVerifiedPageObjectOnPageByID(FuzzyFactorCardID) as FuzzyFactorCard;
+            if (divisionTemplate == null)
             {
+                Console.WriteLine("[ERROR] on Index #{0}, Division Template for Array Snapped In not found on page or in history.", HistoryIndex);
                 return;
             }
-            
-            ParentPage.History.TrashedPageObjects.Remove(snappedInArray);
-            ParentPage.PageObjects.Add(snappedInArray);
-            fuzzyFactorCard.RemoveLastDivision();
+
+            var array = ParentPage.GetVerifiedPageObjectInTrashByID(SnappedInArrayID) as CLPArray;
+            if (array == null)
+            {
+                Console.WriteLine("[ERROR] on Index #{0}, Array for Array Snapped In not found on page or in history.", HistoryIndex);
+                return;
+            }
+
+            ParentPage.History.TrashedPageObjects.Remove(array);
+            ParentPage.PageObjects.Add(array);
+            divisionTemplate.RemoveLastDivision();
         }
 
-        /// <summary>
-        /// Method that will actually redo the action. Already incorporates error checking for existance of ParentPage.
-        /// </summary>
+        /// <summary>Method that will actually redo the action. Already incorporates error checking for existance of ParentPage.</summary>
         protected override void RedoAction(bool isAnimationRedo)
         {
-            var fuzzyFactorCard = ParentPage.GetVerifiedPageObjectOnPageByID(FuzzyFactorCardID) as FuzzyFactorCard;
-            var snappedInArray = ParentPage.GetVerifiedPageObjectOnPageByID(SnappedInArrayID) as CLPArray;
-            if(fuzzyFactorCard == null ||
-               snappedInArray == null)
+            var divisionTemplate = ParentPage.GetVerifiedPageObjectOnPageByID(FuzzyFactorCardID) as FuzzyFactorCard;
+            if (divisionTemplate == null)
             {
+                Console.WriteLine("[ERROR] on Index #{0}, Division Template for Array Snapped In not found on page or in history.", HistoryIndex);
                 return;
             }
 
-            ParentPage.PageObjects.Remove(snappedInArray);
-            ParentPage.History.TrashedPageObjects.Add(snappedInArray);
-            fuzzyFactorCard.SnapInArray(snappedInArray.Columns);
+            var array = ParentPage.GetVerifiedPageObjectOnPageByID(SnappedInArrayID) as CLPArray;
+            if (array == null)
+            {
+                Console.WriteLine("[ERROR] on Index #{0}, Array for Array Snapped In not found on page or in history.", HistoryIndex);
+                return;
+            }
+
+            ParentPage.PageObjects.Remove(array);
+            ParentPage.History.TrashedPageObjects.Add(array);
+            divisionTemplate.SnapInArray(array.Columns);
         }
 
-        /// <summary>
-        /// Method that prepares a clone of the <see cref="IHistoryItem" /> so that it can call Redo() when sent to another machine.
-        /// </summary>
+        /// <summary>Method that prepares a clone of the <see cref="IHistoryItem" /> so that it can call Redo() when sent to another machine.</summary>
         public override IHistoryItem CreatePackagedHistoryItem()
         {
             var clonedHistoryItem = Clone() as FFCArraySnappedInHistoryItem;
             return clonedHistoryItem;
         }
 
-        /// <summary>
-        /// Method that unpacks the <see cref="IHistoryItem" /> after it has been sent to another machine.
-        /// </summary>
+        /// <summary>Method that unpacks the <see cref="IHistoryItem" /> after it has been sent to another machine.</summary>
         public override void UnpackHistoryItem() { }
 
         #endregion //Methods
