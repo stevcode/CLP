a24f040b93fd60fab78f2356d71324a96a458323
stev <stev.code@gmail.com>
2015-05-25 08:48:57 -0400

    refactor of GridDisplay and created ColumnDisplay.

--- CLP.Entities/DomainModel/Displays/ColumnDisplay.cs
+++ CLP.Entities/DomainModel/Displays/ColumnDisplay.cs
@@ -4,21 +4,21 @@ using System.Runtime.Serialization;
 namespace CLP.Entities
 {
     [Serializable]
-    public class GridDisplay : ADisplayBase
+    public class ColumnDisplay : ADisplayBase
     {
         #region Constructors
 
         /// <summary>Initializes <see cref="GridDisplay" /> from scratch.</summary>
-        public GridDisplay() { }
+        public ColumnDisplay() { }
 
         /// <summary>Initializes <see cref="GridDisplay" /> from parent <see cref="Notebook" />.</summary>
-        public GridDisplay(Notebook notebook)
+        public ColumnDisplay(Notebook notebook)
             : base(notebook) { }
 
         /// <summary>Initializes <see cref="GridDisplay" /> based on <see cref="SerializationInfo" />.</summary>
         /// <param name="info"><see cref="SerializationInfo" /> that contains the information.</param>
         /// <param name="context"><see cref="StreamingContext" />.</param>
-        public GridDisplay(SerializationInfo info, StreamingContext context)
+        public ColumnDisplay(SerializationInfo info, StreamingContext context)
             : base(info, context) { }
 
         #endregion //Constructors
