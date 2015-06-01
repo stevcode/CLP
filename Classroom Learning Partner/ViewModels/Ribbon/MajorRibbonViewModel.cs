03c119ff073f3da74beaf251753a03fd3e882447
stev <stev.code@gmail.com>
2015-06-01 08:14:40 -0400

    Button change for deployment

--- Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
+++ Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
@@ -767,9 +767,9 @@ namespace Classroom_Learning_Partner.ViewModels
             //Buttons.Add(_insertProtractorButton);
 
             // Insert Text Box
-            Buttons.Add(Separater);
-            Buttons.Add(_insertImageButton);
-            Buttons.Add(_insertTextBoxButton);
+            //Buttons.Add(Separater);
+            //Buttons.Add(_insertImageButton);
+            //Buttons.Add(_insertTextBoxButton);
             //Buttons.Add(_insertMultipleChoiceTextBoxButton);
             
         }
