51ae0c1bb0f77a6fc952e595df11772ceb2f0ff7
stev <stev.code@gmail.com>
2015-06-01 08:41:16 -0400

    reimplement student login via opening notebook

--- Classroom Learning Partner/ViewModels/BackStage/OpenNotebookPaneViewModel.cs
+++ Classroom Learning Partner/ViewModels/BackStage/OpenNotebookPaneViewModel.cs
@@ -121,25 +121,25 @@ namespace Classroom_Learning_Partner.ViewModels
         {
             PleaseWaitHelper.Show(() => LoadedNotebookService.OpenLocalNotebook(SelectedNotebook, SelectedCacheDirectory, IsIncludeSubmissionsChecked), null, "Loading Notebook");
 
-            //if (App.MainWindowViewModel.CurrentProgramMode != ProgramModes.Student)
-            //{
-            //    return;
-            //}
+            if (App.MainWindowViewModel.CurrentProgramMode != ProgramModes.Student)
+            {
+                return;
+            }
 
-            //if (App.Network.InstructorProxy == null)
-            //{
-            //    return;
-            //}
+            if (App.Network.InstructorProxy == null)
+            {
+                return;
+            }
 
-            //var connectionString = App.Network.InstructorProxy.StudentLogin(App.MainWindowViewModel.CurrentUser.FullName,
-            //                                         App.MainWindowViewModel.CurrentUser.ID,
-            //                                         App.Network.CurrentMachineName,
-            //                                         App.Network.CurrentMachineAddress);
+            var connectionString = App.Network.InstructorProxy.StudentLogin(App.MainWindowViewModel.CurrentUser.FullName,
+                                                     App.MainWindowViewModel.CurrentUser.ID,
+                                                     App.Network.CurrentMachineName,
+                                                     App.Network.CurrentMachineAddress);
 
-            //if (connectionString == "connected")
-            //{
-            //    App.MainWindowViewModel.MajorRibbon.ConnectionStatus = ConnectionStatuses.LoggedIn;
-            //}
+            if (connectionString == "connected")
+            {
+                App.MainWindowViewModel.MajorRibbon.ConnectionStatus = ConnectionStatuses.LoggedIn;
+            }
         }
 
         private bool OnOpenNotebookCanExecute() { return SelectedNotebook != null; }
