7da0c3e0b5e569f0962b969e4055e6fc57ec4e33
stev <stev.code@gmail.com>
2015-06-23 20:02:15 -0400

    Button Mods for Renee's class.

--- Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
+++ Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
@@ -873,32 +873,32 @@ namespace Classroom_Learning_Partner.ViewModels
             Buttons.Add(_setSelectModeButton);
             Buttons.Add(_setDrawModeButton);
             Buttons.Add(_setEraseModeButton);
-            Buttons.Add(_setMarkModeButton);
-            Buttons.Add(Separater);
-            Buttons.Add(_setLassoModeButton);
-            Buttons.Add(_setCutModeButton);
+            //Buttons.Add(_setMarkModeButton);
+            //Buttons.Add(Separater);
+            //Buttons.Add(_setLassoModeButton);
+            //Buttons.Add(_setCutModeButton);
             //Buttons.Add(_setDividerCreationModeButton);
 
             // Insert Math Tools
             Buttons.Add(Separater);
             Buttons.Add(_insertGeneralStampButton);
             //Buttons.Add(_insertGroupStampButton);
-            Buttons.Add(_insertNumberLineButton);
-            Buttons.Add(_insertAutoNumberLineButton);
-            Buttons.Add(_insertArrayButton);
+            //Buttons.Add(_insertNumberLineButton);
+            //Buttons.Add(_insertAutoNumberLineButton);
+            //Buttons.Add(_insertArrayButton);
             //Buttons.Add(_insert10x10ArrayButton);
             //Buttons.Add(_insertArrayCardButton);
             //Buttons.Add(_insertFactorCardButton);
             //Buttons.Add(_insertObscurableArrayButton);
-            Buttons.Add(_insertPileButton);
+            //Buttons.Add(_insertPileButton);
             //Buttons.Add(_insertDivisionTemplateButton);
-            Buttons.Add(_insertBinButton);
+            //Buttons.Add(_insertBinButton);
 
             // Insert Shapes
-            //Buttons.Add(Separater);
-            //Buttons.Add(_insertSquareButton);
-            //Buttons.Add(_insertCircleButton);
-            //Buttons.Add(_insertTriangleButton);
+            Buttons.Add(Separater);
+            Buttons.Add(_insertSquareButton);
+            Buttons.Add(_insertCircleButton);
+            Buttons.Add(_insertTriangleButton);
             //Buttons.Add(_insertHorizontalLineButton);
             //Buttons.Add(_insertVerticalLineButton);
             //Buttons.Add(_insertProtractorButton);
