aa904d2b7ff40972651d51391a5619abd243a7a3
stev <stev.code@gmail.com>
2015-08-30 09:05:59 -0400

    array divide and snap actions

--- CLP.Entities/DomainModel/HistoryAnalysis/HistoryActions/ArrayHistoryAction.cs
+++ CLP.Entities/DomainModel/HistoryAnalysis/HistoryActions/ArrayHistoryAction.cs
@@ -23,20 +23,6 @@ namespace CLP.Entities
 
         #endregion //Constructors
 
-        #region Calculated Properties
-
-        public List<PageObjectCutHistoryItem> ArrayCutActions
-        {
-            get { return HistoryItems.OfType<PageObjectCutHistoryItem>().ToList(); }
-        }
-
-        public List<CLPArrayDivisionsChangedHistoryItem> ArrayDivisionActions
-        {
-            get { return HistoryItems.OfType<CLPArrayDivisionsChangedHistoryItem>().ToList();}
-        }
-
-        #endregion //Calculated Properties
-
         #region Static Methods
 
         public static ArrayHistoryAction VerifyAndGenerate(CLPPage parentPage, List<IHistoryItem> historyItems)
@@ -55,6 +41,8 @@ namespace CLP.Entities
             var snapHistoryItems = GetArraySnapHistoryItems(historyItems);
             var divideHistoryItems = GetArrayDividedHistoryItems(historyItems);
             var historyItemCount = historyItems.Count;
+
+            #region ACTION_ARRAY_ROTATE
 
             if (rotateHistoryItems.Count == historyItemCount &&
                 rotateHistoryItems.Count == 1)
@@ -82,13 +70,17 @@ namespace CLP.Entities
                 return historyAction;
             }
 
+            #endregion // ACTION_ARRAY_ROTATE
+
+            #region ACTION_ARRAY_CUT
+
             if (cutHistoryItems.Count == historyItemCount &&
                 cutHistoryItems.Count == 1)
             {
                 var historyItem = cutHistoryItems.First();
 
                 var cutArrayID = historyItem.CutPageObjectID;
-                var cutArray = parentPage.GetPageObjectByIDOnPageOrInHistory(cutArrayID);
+                var cutArray = parentPage.GetPageObjectByIDOnPageOrInHistory(cutArrayID) as CLPArray;
                 if (cutArray == null)
                 {
                     return null;
@@ -142,6 +134,120 @@ namespace CLP.Entities
 
                 return historyAction;
             }
+
+            #endregion // ACTION_ARRAY_CUT
+
+            #region ACTION_ARRAY_SNAP
+
+            if (snapHistoryItems.Count == historyItemCount &&
+                snapHistoryItems.Count == 1)
+            {
+                var historyItem = snapHistoryItems.First();
+                var codedObject = Codings.OBJECT_ARRAY;
+                var persistingArray = parentPage.GetPageObjectByIDOnPageOrInHistory(historyItem.PersistingArrayID) as CLPArray;
+                var snappedArray = parentPage.GetPageObjectByIDOnPageOrInHistory(historyItem.SnappedArrayID) as CLPArray;
+                if (persistingArray == null ||
+                    snappedArray == null)
+                {
+                    return null;
+                }
+
+                // For consistency's sake, the persistingArray is always the CodedID.
+                // It is the array that remains on the page, therefore it is the one
+                // whose dimensions change. The SubID is the array that is snapped onto
+                // the persistingArray then disappears.
+                var codedID = persistingArray.GetCodedIDAtHistoryIndex(historyItem.HistoryIndex);
+                var incrementID = GetIncrementID(codedObject, codedID);
+                var codedSubID = snappedArray.GetCodedIDAtHistoryIndex(historyItem.HistoryIndex);
+                var incrementSubID = GetIncrementID(codedObject, codedSubID);
+                var codedActionID = persistingArray.GetCodedIDAtHistoryIndex(historyItem.HistoryIndex + 1);
+                var codedActionIDIncrementID = IncrementAndGetIncrementID(codedObject, codedActionID);
+                if (!string.IsNullOrWhiteSpace(codedActionIDIncrementID))
+                {
+                    codedActionID += " " + codedActionIDIncrementID;
+                }
+
+                var historyAction = new ArrayHistoryAction(parentPage, historyItems)
+                {
+                    CodedObject = codedObject,
+                    CodedObjectAction = Codings.ACTION_ARRAY_SNAP,
+                    CodedObjectID = codedID,
+                    CodedObjectIDIncrement = incrementID,
+                    CodedObjectSubID = codedSubID,
+                    CodedObjectSubIDIncrement = incrementSubID,
+                    CodedObjectActionID = codedActionID
+                };
+
+                return historyAction;
+            }
+
+            #endregion // ACTION_ARRAY_SNAP
+
+            #region ACTION_ARRAY_DIVIDE
+
+            if (divideHistoryItems.Count == historyItemCount &&
+                divideHistoryItems.Count == 1)
+            {
+                var historyItem = divideHistoryItems.First();
+                var codedObject = Codings.OBJECT_ARRAY;
+                var dividedArrayID = historyItem.ArrayID;
+                var dividedArray = parentPage.GetPageObjectByIDOnPageOrInHistory(dividedArrayID) as CLPArray;
+                if (dividedArray == null ||
+                    historyItem.IsColumnRegionsChange == null)
+                {
+                    return null;
+                }
+
+                var codedID = dividedArray.GetCodedIDAtHistoryIndex(historyItem.HistoryIndex);
+                var incrementID = GetIncrementID(codedObject, codedID);
+                var codedActionSegments = new List<string>();
+
+                // QUESTION: Right now, only listing new regions created. Alternatively, can list all regions, or have SubID for the replaced region
+                foreach (var region in historyItem.NewRegions)
+                {
+                    int regionRow;
+                    int regionColumn;
+                    if (historyItem.IsColumnRegionsChange.Value)
+                    {
+                        regionColumn = (int)dividedArray.GetColumnsAndRowsAtHistoryIndex(historyItem.HistoryIndex).X;
+                        regionRow = region.Value;
+                    }
+                    else
+                    {
+                        regionColumn = region.Value;
+                        regionRow = (int)dividedArray.GetColumnsAndRowsAtHistoryIndex(historyItem.HistoryIndex).Y;
+                    }
+
+                    var segmentID = string.Format("{0}x{1}", regionColumn, regionRow);
+                    var segmentIncrementID = IncrementAndGetIncrementID(codedObject, segmentID); // TODO: add something like "_SUB to codedObject to give subarrays their own increment path
+                    if (!string.IsNullOrWhiteSpace(segmentIncrementID))
+                    {
+                        segmentID += " " + segmentIncrementID;
+                    }
+
+                    codedActionSegments.Add(segmentID);
+                }
+
+                if (historyItem.IsColumnRegionsChange.Value)
+                {
+                    codedActionSegments.Add("v");
+                }
+
+                var codedActionID = string.Join(", ", codedActionSegments);
+
+                var historyAction = new ArrayHistoryAction(parentPage, historyItems)
+                {
+                    CodedObject = codedObject,
+                    CodedObjectAction = Codings.ACTION_ARRAY_DIVIDE,
+                    CodedObjectID = codedID,
+                    CodedObjectIDIncrement = incrementID,
+                    CodedObjectActionID = codedActionID
+                };
+
+                return historyAction;
+            }
+
+            #endregion // ACTION_ARRAY_DIVIDE
 
             return null;
         }
