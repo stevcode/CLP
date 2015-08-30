2e92c6db27ba1d1255363d59b2110e20646cdd62
stev <stev.code@gmail.com>
2015-08-30 17:49:26 -0400

    OBJECT_MOVE completed

--- CLP.Entities/DomainModel/HistoryAnalysis/HistoryActions/GeneralPageObjectAction.cs
+++ CLP.Entities/DomainModel/HistoryAnalysis/HistoryActions/GeneralPageObjectAction.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Ink;
 
 namespace CLP.Entities
 {
@@ -75,9 +76,56 @@ namespace CLP.Entities
             return null;
         }
 
+        public static HistoryAction Move(CLPPage page, List<IHistoryItem> objectsMovedHistoryItems)
+        {
+            if (page == null ||
+               objectsMovedHistoryItems == null ||
+               !objectsMovedHistoryItems.Any())
+            {
+                return null;
+            }
+
+            var movedPageObjects = GetMovedPageObjects(page, objectsMovedHistoryItems);
+            var movedStrokes = GetMovedStrokes(page, objectsMovedHistoryItems);
+
+            if (movedPageObjects.Count > 1 ||
+                movedStrokes.Any()) // Lasso move
+            {
+                // TODO
+                return null;
+            }
+
+            if (movedPageObjects.Count == 1)
+            {
+                var historyIndex = objectsMovedHistoryItems.First().HistoryIndex;
+                var pageObject = movedPageObjects.First();
+                var codedObject = pageObject.CodedName;
+                var codedObjectID = pageObject.GetCodedIDAtHistoryIndex(historyIndex);
+                var historyAction = new HistoryAction(page, objectsMovedHistoryItems)
+                {
+                    CodedObject = codedObject,
+                    CodedObjectAction = Codings.ACTION_OBJECT_MOVE,
+                    CodedObjectID = codedObjectID,
+                    CodedObjectIDIncrement = HistoryAction.GetIncrementID(pageObject.ID, codedObject, codedObjectID)
+                    // TODO: make coded action to describe distance travelled during move.
+                    // note that there may be more than one objectsMovedHistoryItem in a row where
+                    // a student moved the same pageObject several consecutive times.
+                };
+
+                return historyAction;
+            }
+
+            return null;
+        }
+
         public static List<IPageObject> GetMovedPageObjects(CLPPage page, List<IHistoryItem> historyItems)
         {
-            return historyItems.OfType<ObjectsMovedBatchHistoryItem>().SelectMany(h => h.PageObjectIDs.Keys.Select(page.GetPageObjectByIDOnPageOrInHistory)).ToList();
+            return historyItems.OfType<ObjectsMovedBatchHistoryItem>().SelectMany(h => h.PageObjectIDs.Keys.Select(page.GetPageObjectByIDOnPageOrInHistory)).Distinct().ToList();
+        }
+
+        public static List<Stroke> GetMovedStrokes(CLPPage page, List<IHistoryItem> historyItems)
+        {
+            return historyItems.OfType<ObjectsMovedBatchHistoryItem>().SelectMany(h => h.StrokeIDs.Keys.Select(page.GetStrokeByIDOnPageOrInHistory)).Distinct().ToList();
         }
 
         public static List<IPageObject> GetResizedPageObjects(CLPPage page, List<IHistoryItem> historyItems)
