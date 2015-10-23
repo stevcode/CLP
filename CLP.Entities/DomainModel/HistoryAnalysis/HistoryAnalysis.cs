f5a9293e85e6c2865a4954577f11c1cbd9e80486
stev <stev.code@gmail.com>
2015-10-23 12:05:28 -0400

    refactoring

--- CLP.Entities/DomainModel/HistoryAnalysis/HistoryAnalysis.cs
+++ CLP.Entities/DomainModel/HistoryAnalysis/HistoryAnalysis.cs
@@ -2,52 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Ink;
 using Catel.Collections;
 
 namespace CLP.Entities
 {
-    // Derived from: http://www.johndcook.com/blog/standard_deviation/
-    public class RollingStandardDeviation
-    {
-        private int _numberOfDataValues;
-        private double _oldMean;
-        private double _newMean;
-        private double _oldSum;
-        private double _newSum;
-
-        public RollingStandardDeviation() { _numberOfDataValues = 0; }
-
-        public double Mean { get { return _numberOfDataValues > 0 ? _newMean : 0.0; } }
-
-        public double Variance {  get { return _numberOfDataValues > 1 ? _newSum / (_numberOfDataValues - 1) : 0.0; } }
-
-        public double StandardDeviation { get { return Math.Sqrt(Variance); } }
-
-        public void Clear() { _numberOfDataValues = 0; }
-
-        public void Update(double x)
-        {
-            _numberOfDataValues++;
-
-            if (_numberOfDataValues == 1)
-            {
-                _oldMean = x;
-                _newMean = x;
-                _oldSum = 0.0;
-                return;
-            }
-
-            _newMean = _oldMean + (x - _oldMean) / _numberOfDataValues;
-            _newSum = _oldSum + (x - _oldMean) * (x - _oldMean);
-
-            _oldMean = _newMean;
-            _oldSum = _newSum;
-        }
-    }
-
     public static class HistoryAnalysis
     {
         public static void GenerateHistoryActions(CLPPage page)
@@ -77,7 +37,7 @@ namespace CLP.Entities
                 File.Delete(filePath);
             }
             File.WriteAllText(filePath, "");
-            File.AppendAllText(filePath, "PASS [1]"+ "\n");
+            File.AppendAllText(filePath, "PASS [1]" + "\n");
             foreach (var item in initialHistoryActions)
             {
                 var semi = item == initialHistoryActions.Last() ? string.Empty : "; ";
@@ -102,10 +62,10 @@ namespace CLP.Entities
 
             // Third Pass
             page.History.HistoryActions.Add(new HistoryAction(page, new List<IHistoryItem>())
-            {
-                CodedObject = "PASS",
-                CodedObjectID = "3"
-            });
+                                            {
+                                                CodedObject = "PASS",
+                                                CodedObjectID = "3"
+                                            });
             var interpretedHistoryActions = InterpretHistoryActions(page, refinedInkHistoryActions);
             page.History.HistoryActions.AddRange(interpretedHistoryActions);
         }
@@ -154,10 +114,7 @@ namespace CLP.Entities
 
             IHistoryAction historyAction = null;
             TypeSwitch.On(historyItem)
-                      .Case<ObjectsOnPageChangedHistoryItem>(h =>
-                      {
-                          historyAction = ObjectCodedActions.Add(page, h) ?? ObjectCodedActions.Delete(page, h);
-                      })
+                      .Case<ObjectsOnPageChangedHistoryItem>(h => { historyAction = ObjectCodedActions.Add(page, h) ?? ObjectCodedActions.Delete(page, h); })
                       .Case<CLPArrayRotateHistoryItem>(h => { historyAction = ArrayCodedActions.Rotate(page, h); })
                       .Case<PageObjectCutHistoryItem>(h => { historyAction = ArrayCodedActions.Cut(page, h); })
                       .Case<CLPArraySnapHistoryItem>(h => { historyAction = ArrayCodedActions.Snap(page, h); })
@@ -282,9 +239,7 @@ namespace CLP.Entities
             return refinedHistoryActions;
         }
 
-        /// <summary>
-        /// Processes "INK change" action into "INK strokes (erase) [ID: location RefObject [RefObjectID]]" actions
-        /// </summary>
+        /// <summary>Processes "INK change" action into "INK strokes (erase) [ID: location RefObject [RefObjectID]]" actions</summary>
         /// <param name="page">Parent page the history action belongs to.</param>
         /// <param name="historyAction">"INK change" history action to process</param>
         public static List<IHistoryAction> ProcessInkChangeHistoryAction(CLPPage page, IHistoryAction historyAction)
@@ -367,8 +322,7 @@ namespace CLP.Entities
                             var nextCentroid = nextStrokeCopy.WeightedCenter();
                             var distanceFromLastStroke = Math.Sqrt(InkCodedActions.DistanceSquaredBetweenPoints(currentCentroid, nextCentroid));
                             var lastStrokeDistanceZScore = (distanceFromLastStroke - averageClosestStrokeDistance) / closestStrokeDistanceStandardDeviation;
-                            var isCloseToLastStroke = lastStrokeDistanceZScore <= MAX_DISTANCE_Z_SCORE ||
-                                                      distanceFromLastStroke <= averageStrokeDimensions.X * DIMENSION_MULTIPLIER_THRESHOLD ||
+                            var isCloseToLastStroke = lastStrokeDistanceZScore <= MAX_DISTANCE_Z_SCORE || distanceFromLastStroke <= averageStrokeDimensions.X * DIMENSION_MULTIPLIER_THRESHOLD ||
                                                       distanceFromLastStroke <= averageStrokeDimensions.Y * DIMENSION_MULTIPLIER_THRESHOLD;
 
                             bool isCloseToCluster;
@@ -383,8 +337,7 @@ namespace CLP.Entities
                                 isCloseToCluster = clusterDistanceZScore <= MAX_DISTANCE_Z_SCORE;
                             }
 
-                            if (isCloseToLastStroke ||
-                                isCloseToCluster)
+                            if (isCloseToLastStroke || isCloseToCluster)
                             {
                                 isNextPartOfCurrentCluster = true;
 
@@ -417,8 +370,7 @@ namespace CLP.Entities
                         isNextPageObjectReferencePartOfCurrent = nextPageObjectReference.ID == currentPageObjectReference.ID;
                     }
                     var isNextLocationReferencePartOfCurrent = nextLocationReference == currentLocationReference;
-                    if (isNextInkPartOfCurrent &&
-                        (isNextPartOfCurrentCluster || (currentLocationReference == Codings.ACTIONID_INK_LOCATION_OVER && isNextPageObjectReferencePartOfCurrent)))
+                    if (isNextInkPartOfCurrent && (isNextPartOfCurrentCluster || (currentLocationReference == Codings.ACTIONID_INK_LOCATION_OVER && isNextPageObjectReferencePartOfCurrent)))
                     {
                         continue;
                     }
