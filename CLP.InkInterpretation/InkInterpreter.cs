dc3843db301c04c34c155229b83212983560b213
stev <stev.code@gmail.com>
2016-02-02 09:31:57 -0500

    Added ink interpreter method to return top 3 best guesses

--- CLP.InkInterpretation/InkInterpreter.cs
+++ CLP.InkInterpretation/InkInterpreter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Ink;
 using Windows.Foundation;
@@ -56,6 +57,62 @@ namespace CLP.InkInterpretation
             {
                 return string.Empty;
             }
+        }
+
+        public static List<string> StrokesToAllGuessesText(StrokeCollection strokes)
+        {
+            if (strokes == null ||
+                !strokes.Any())
+            {
+                return new List<string>();
+            }
+
+            var inkManager = new InkManager();
+            var strokeBuilder = new InkStrokeBuilder();
+
+            //Convert System.Windows.Ink.Stroke to Windows.UI.Input.Inking.InkStroke and add to InkManager
+            foreach (var stroke in strokes.Select(s => s.StylusPoints.Select(sp => new Point(sp.X, sp.Y))).Select(pts => strokeBuilder.CreateStroke(pts)))
+            {
+                inkManager.AddStroke(stroke);
+            }
+
+            //Set handwriting recognizer to US English
+            const string RECOGNIZER_NAME = "Microsoft English (US) Handwriting Recognizer";
+            var recognizers = inkManager.GetRecognizers();
+            for (int i = 0, len = recognizers.Count; i < len; i++)
+            {
+                if (RECOGNIZER_NAME == recognizers[i].Name)
+                {
+                    inkManager.SetDefaultRecognizer(recognizers[i]);
+                    break;
+                }
+            }
+
+            //Asynchronously interpret strokes as text.
+            var recognitionTask = inkManager.RecognizeAsync(InkRecognitionTarget.All).AsTask();
+            var recognitionResults = recognitionTask.Result;
+
+            //Doing a recognition does not update the storage of results (the results that are stored inside the ink manager). 
+            //We do that ourselves by calling this below method.
+            inkManager.UpdateRecognitionResults(recognitionResults);
+
+            // Aggregate the 3 most likely result words, with spaces between.
+            var allInterpretations = new List<string>();
+            for (var i = 0; i < 3; i++)
+            {
+                try
+                {
+                    var interpretation = recognitionResults.Select(result => result.GetTextCandidates()).Aggregate(string.Empty, (current, text) => current + " " + text[i]);
+                    var trimmedInterpretation = interpretation.Trim();
+                    allInterpretations.Add(trimmedInterpretation);
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+
+            return allInterpretations;
         }
     }
 }
