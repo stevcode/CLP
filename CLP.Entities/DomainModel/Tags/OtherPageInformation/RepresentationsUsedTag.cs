82bbbc95347aff05d2412c9658cd54dfb683ae27
stev <stev.code@gmail.com>
2016-02-02 22:43:45 -0500

    New format for Representations Used Tag

--- CLP.Entities/DomainModel/Tags/OtherPageInformation/RepresentationsUsedTag.cs
+++ CLP.Entities/DomainModel/Tags/OtherPageInformation/RepresentationsUsedTag.cs
@@ -12,64 +12,64 @@ namespace CLP.Entities
         #region Constructors
 
         /// <summary>Initializes <see cref="RepresentationsUsedTag" /> from scratch.</summary>
-        public RepresentationsUsedTag()
-        { }
+        public RepresentationsUsedTag() { }
 
         /// <summary>Initializes <see cref="RepresentationsUsedTag" /> from values.</summary>
         /// <param name="parentPage">The <see cref="CLPPage" /> the <see cref="ObjectTypesOnPageTag" /> belongs to.</param>
         /// <param name="origin"></param>
         /// <param name="currentUserID"></param>
-        public RepresentationsUsedTag(CLPPage parentPage, Origin origin, List<string> representations)
+        public RepresentationsUsedTag(CLPPage parentPage, Origin origin, List<string> allRepresentations, List<string> deletedCodedRepresentations, List<string> finalCodedRepresentations)
             : base(parentPage, origin)
         {
             IsSingleValueTag = true;
 
-            Representations = representations;
+            AllRepresentations = allRepresentations;
+            DeletedCodedRepresentations = deletedCodedRepresentations;
+            FinalCodedRepresentations = finalCodedRepresentations;
         }
 
         /// <summary>Initializes <see cref="RepresentationsUsedTag" /> based on <see cref="SerializationInfo" />.</summary>
         /// <param name="info"><see cref="SerializationInfo" /> that contains the information.</param>
         /// <param name="context"><see cref="StreamingContext" />.</param>
         public RepresentationsUsedTag(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        { }
+            : base(info, context) { }
 
         #endregion //Constructors
 
         #region Properties
 
-        /// <summary>List of all the representations of the relevant <see cref="IPageObject" />s on the <see cref="CLPPage" />.</summary>
-        public List<string> Representations
+        /// <summary>Distinct list of all representations used through the history of the page.</summary>
+        public List<string> AllRepresentations
         {
-            get { return GetValue<List<string>>(RepresentationsProperty); }
-            set { SetValue(RepresentationsProperty, value); }
+            get { return GetValue<List<string>>(AllRepresentationsProperty); }
+            set { SetValue(AllRepresentationsProperty, value); }
         }
 
-        public static readonly PropertyData RepresentationsProperty = RegisterProperty("Representations", typeof(List<string>));
+        public static readonly PropertyData AllRepresentationsProperty = RegisterProperty("AllRepresentations", typeof (List<string>), () => new List<string>());
 
-        public List<string> FinalRepresentations
+        /// <summary>Coded values for all deleted representations.</summary>
+        public List<string> DeletedCodedRepresentations
         {
-            get
-            {
-                if (ParentPage == null)
-                {
-                    return new List<string>();
-                }
+            get { return GetValue<List<string>>(DeletedCodedRepresentationsProperty); }
+            set { SetValue(DeletedCodedRepresentationsProperty, value); }
+        }
 
-                var representations = ParentPage.PageObjects.Where(HistoryAnalysis.IsRepresentation).Select(p => p.CodedName).Distinct().ToList();
+        public static readonly PropertyData DeletedCodedRepresentationsProperty = RegisterProperty("DeletedCodedRepresentations", typeof (List<string>), () => new List<string>());
 
-                return representations;
-            }
-        } 
+        /// <summary>Coded values for all final representations.</summary>
+        public List<string> FinalCodedRepresentations
+        {
+            get { return GetValue<List<string>>(FinalCodedRepresentationsProperty); }
+            set { SetValue(FinalCodedRepresentationsProperty, value); }
+        }
+
+        public static readonly PropertyData FinalCodedRepresentationsProperty = RegisterProperty("FinalCodedRepresentations", typeof (List<string>), () => new List<string>());
 
         #region ATagBase Overrides
 
         public string AnalysisCode
         {
-            get
-            {
-                return string.Format("MR [{0}]", string.Join(", ", Representations));
-            }
+            get { return string.Format("MR [{0}]", string.Join(", ", AllRepresentations)); }
         }
 
         public override Category Category
@@ -86,23 +86,16 @@ namespace CLP.Entities
         {
             get
             {
-                var finalRepresentations = FinalRepresentations;
-                var friendlyRepresentations =
-                    Representations.Select(r => finalRepresentations.Contains(r) ? string.Format("{0}, Final Representation", Codings.FriendlyObjects[r]) : Codings.FriendlyObjects[r]).ToList();
-                if (!friendlyRepresentations.Any())
+                if (!AllRepresentations.Any())
                 {
                     var isStrokesUsed = ParentPage.InkStrokes.Any() || ParentPage.History.TrashedInkStrokes.Any();
-                    if (isStrokesUsed)
-                    {
-                        friendlyRepresentations.Add("Ink Only");
-                    }
-                    else
-                    {
-                        friendlyRepresentations.Add("Blank Page");
-                    }
+                    return isStrokesUsed ? "Ink Only" : "Blank Page";
                 }
-                var codeString = Representations.Count > 1 ? string.Format("\nCode: {0}", AnalysisCode) : string.Empty;
-                return string.Format("{0}{1}", string.Join("\n", friendlyRepresentations), codeString);
+
+                var deletedSection = !DeletedCodedRepresentations.Any() ? string.Empty : string.Format("Deleted Representation(s):\n{0}", string.Join("\n", DeletedCodedRepresentations));
+                var finalSection = !FinalCodedRepresentations.Any() ? string.Empty : string.Format("Final Representation(s):\n{0}", string.Join("\n", FinalCodedRepresentations));
+                var codeSection = AllRepresentations.Count > 1 ? string.Format("\nCode: {0}", AnalysisCode) : string.Empty;
+                return string.Format("{0}{1}{2}", deletedSection, finalSection, codeSection);
             }
         }
 
