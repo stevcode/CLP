a48e56168ad03fa406745091563df8b4d080d41a
stev <stev.code@gmail.com>
2016-02-12 07:31:13 -0500

    Generic OPTICS algorithm

---
+++ MachineAnalysis/PointList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CLP.MachineAnalysis
+{
+    public class PointsList
+    {
+        internal List<Point> _points;
+
+        public PointsList()
+        {
+            _points = new List<Point>();
+        }
+
+        public void AddPoint(uint id, double[] vector)
+        {
+            var newPoint = new Point((uint)_points.Count, id, vector);
+            _points.Add(newPoint);
+        }
+    }
+}
---
+++ MachineAnalysis/PointReachability.cs
@@ -0,0 +1,14 @@
+﻿namespace CLP.MachineAnalysis
+{
+    public struct PointReachability
+    {
+        public PointReachability(uint pointId, double reachability)
+        {
+            PointId = pointId;
+            Reachability = reachability;
+        }
+
+        public readonly uint PointId;
+        public readonly double Reachability;
+    }
+}
