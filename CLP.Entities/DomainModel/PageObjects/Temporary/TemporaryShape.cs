62da6925c4b2569f86b812d939d4e6b6a4a56a47
Alex Markovits <akmarkov@mit.edu>
2016-04-10 10:13:27 -0400

    include required files

---
+++ CLP.Entities/DomainModel/PageObjects/Temporary/TemporaryShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Catel.Data;
+
+namespace CLP.Entities
+{
+    public class TemporaryShape : Shape
+    {
+        public TemporaryShape(CLPPage parentPage, ShapeType shapeType)
+            : base(parentPage, shapeType)
+        {
+            
+        }
+    }
+}
