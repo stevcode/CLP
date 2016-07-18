0f2a4b42675cd84f8f09875c79b4957ef802b10f
stev <stev.code@gmail.com>
2016-07-18 10:14:53 -0400

    button changes

--- Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
+++ Classroom Learning Partner/ViewModels/Ribbon/MajorRibbonViewModel.cs
@@ -1001,12 +1001,12 @@ namespace Classroom_Learning_Partner.ViewModels
             Buttons.Add(_insertPileButton);
 
             // Insert Shapes
-            //Buttons.Add(Separater);
-            //Buttons.Add(_insertSquareButton);
-            //Buttons.Add(_insertCircleButton);
+            Buttons.Add(Separater);
+            Buttons.Add(_insertSquareButton);
+            Buttons.Add(_insertCircleButton);
             //Buttons.Add(_insertTriangleButton);
-            //Buttons.Add(_insertHorizontalLineButton);
-            //Buttons.Add(_insertVerticalLineButton);
+            Buttons.Add(_insertHorizontalLineButton);
+            Buttons.Add(_insertVerticalLineButton);
             //Buttons.Add(_insertProtractorButton);
             //Buttons.Add(_insertRightDiagonalButton);
             //Buttons.Add(_insertRightDiagonalDashedButton);
@@ -1014,11 +1014,11 @@ namespace Classroom_Learning_Partner.ViewModels
             //Buttons.Add(_insertLeftDiagonalDashedButton);
 
             // Insert Text Box
-            //Buttons.Add(Separater);
-            //Buttons.Add(_insertImageButton);
-            //Buttons.Add(_insertTextBoxButton);
-            //Buttons.Add(_insertRecognitionRegionButton);
-            //Buttons.Add(_insertMultipleChoiceTextBoxButton);
+            Buttons.Add(Separater);
+            Buttons.Add(_insertImageButton);
+            Buttons.Add(_insertTextBoxButton);
+            Buttons.Add(_insertRecognitionRegionButton);
+            Buttons.Add(_insertMultipleChoiceTextBoxButton);
         }
 
         #endregion //Methods
