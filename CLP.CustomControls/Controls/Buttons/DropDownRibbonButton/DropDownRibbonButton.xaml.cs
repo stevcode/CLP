7c3b4dad3d35eb2153baabb74f7cf15d43759c6d
stev <stev.code@gmail.com>
2016-07-27 00:31:55 -0400

    added scaffold code for drop down button

---
+++ CLP.CustomControls/Controls/Buttons/DropDownRibbonButton/DropDownRibbonButton.xaml.cs
@@ -0,0 +1,114 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Based on code from WildGums:
+// https://github.com/WildGums/Orc.Controls/blob/develop/src/Orc.Controls/Orc.Controls.Shared/Controls/DropDownButton/Views/DropDownButton.xaml.cs
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Catel;
+
+namespace CLP.CustomControls
+{
+
+    /// <summary>
+    /// Interaction logic for DropDownRibbonButton.xaml
+    /// </summary>
+    public partial class DropDownRibbonButton
+    {
+        #region Constructors
+        public DropDownRibbonButton()
+        {
+            InitializeComponent();
+
+            LayoutUpdated += OnLayoutUpdated;
+        }
+        #endregion
+
+        #region Properties
+        public object Header
+        {
+            get { return (object)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(object),
+            typeof(DropDownRibbonButton), new FrameworkPropertyMetadata(string.Empty));
+
+        public ContextMenu DropDown
+        {
+            get { return (ContextMenu)GetValue(DropDownProperty); }
+            set { SetValue(DropDownProperty, value); }
+        }
+
+        public static readonly DependencyProperty DropDownProperty = DependencyProperty.Register("DropDown", typeof(ContextMenu),
+            typeof(DropDownRibbonButton), new UIPropertyMetadata(null));
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command", typeof(ICommand),
+            typeof(DropDownRibbonButton), new UIPropertyMetadata(null));
+
+        public Brush AccentColorBrush
+        {
+            get { return (Brush)GetValue(AccentColorBrushProperty); }
+            set { SetValue(AccentColorBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty AccentColorBrushProperty = DependencyProperty.Register("AccentColorBrush", typeof(Brush),
+            typeof(DropDownRibbonButton), new PropertyMetadata(Brushes.LightGray, (sender, e) => ((DropDownRibbonButton)sender).OnAccentColorBrushChanged()));
+
+        public bool ShowDefaultButton
+        {
+            get { return (bool)GetValue(ShowDefaultButtonProperty); }
+            set { SetValue(ShowDefaultButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowDefaultButtonProperty = DependencyProperty.Register("ShowDefaultButton", typeof(bool),
+            typeof(DropDownRibbonButton), new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public bool EnableTransparentBackground
+        {
+            get { return (bool)GetValue(EnableTransparentBackgroundProperty); }
+            set { SetValue(EnableTransparentBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty EnableTransparentBackgroundProperty = DependencyProperty.Register("EnableTransparentBackground", typeof(bool),
+            typeof(DropDownRibbonButton), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        #endregion
+
+        #region Events
+        public event EventHandler<EventArgs> ContentLayoutUpdated;
+        #endregion
+
+        #region Methods
+        private void OnAccentColorBrushChanged()
+        {
+            var solidColorBrush = AccentColorBrush as SolidColorBrush;
+            if (solidColorBrush != null)
+            {
+                var accentColor = ((SolidColorBrush)AccentColorBrush).Color;
+                accentColor.CreateAccentColorResourceDictionary("DropDownButton");
+            }
+        }
+
+        private void OnLayoutUpdated(object sender, EventArgs e)
+        {
+            ContentLayoutUpdated.SafeInvoke(this, e);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            AccentColorBrush = TryFindResource("AccentColorBrush") as SolidColorBrush;
+        }
+        #endregion
+    }
+}
---
+++ CLP.CustomControls/Controls/Buttons/DropDownRibbonButton/DropDownRibbonButtonBehavior.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using Catel.Windows.Interactivity;
+using Catel.Windows.Threading;
+
+namespace CLP.CustomControls
+{
+    public class DropDownButtonBehavior : BehaviorBase<DropDownRibbonButton>
+    {
+        #region Methods
+        protected override void OnAssociatedObjectLoaded()
+        {
+            base.OnAssociatedObjectLoaded();
+
+            var binding = new Binding("DropDown.IsOpen")
+            {
+                Source = AssociatedObject,
+                Mode = BindingMode.TwoWay
+            };
+
+            AssociatedObject.SetBinding(ToggleButton.IsCheckedProperty, binding);
+
+            AssociatedObject.ToggleButton.Click += OnClick;
+
+            var dropDown = AssociatedObject.DropDown;
+            if (dropDown != null)
+            {
+                dropDown.Closed += OnDropDownClosed;
+            }
+        }
+
+        protected override void OnAssociatedObjectUnloaded()
+        {
+            base.OnAssociatedObjectUnloaded();
+
+            AssociatedObject.ToggleButton.Click -= OnClick;
+
+            var dropDown = AssociatedObject.DropDown;
+            if (dropDown != null)
+            {
+                dropDown.Closed -= OnDropDownClosed;
+            }
+        }
+
+        private void OnDropDownClosed(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.ToggleButton.IsChecked = AssociatedObject.DropDown.IsOpen;
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            var dropDown = AssociatedObject.DropDown;
+            if (dropDown != null && (AssociatedObject.ToggleButton.IsChecked ?? false))
+            {
+                dropDown.Dispatcher.BeginInvoke(() =>
+                {
+                    dropDown.PlacementTarget = AssociatedObject;
+                    dropDown.Placement = PlacementMode.Custom;
+                    dropDown.MinWidth = AssociatedObject.ActualWidth;
+                    dropDown.CustomPopupPlacementCallback = CustomPopupPlacementCallback;
+                });
+
+                dropDown.IsOpen = true;
+            }
+        }
+
+        private static CustomPopupPlacement[] CustomPopupPlacementCallback(Size popupSize, Size targetSize, Point offset)
+        {
+            var p = new Point
+            {
+                Y = targetSize.Height - offset.Y,
+                X = -offset.X
+            };
+
+            return new[]
+            {
+                new CustomPopupPlacement(p, PopupPrimaryAxis.Horizontal)
+            };
+        }
+        #endregion
+    }
+}
