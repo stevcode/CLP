46c0f6d4eba38b49addf1bdfbffde1a0055f7932
stev <stev.code@gmail.com>
2016-08-05 00:16:17 -0400

    FrameworkElement and Visual extensions

---
+++ Classroom Learning Partner/Utilities/Extensions/VisualExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Media;
+
+namespace Classroom_Learning_Partner
+{
+    public static class VisualExtensions
+    {
+        public static T GetVisualChild<T>(this Visual visual) where T : Visual
+        {
+            var child = default(T);
+            var numVisuals = VisualTreeHelper.GetChildrenCount(visual);
+            for (var i = 0; i < numVisuals; i++)
+            {
+                var v = (Visual)VisualTreeHelper.GetChild(visual, i);
+                child = v as T ?? GetVisualChild<T>(v);
+                if (child != null)
+                {
+                    break;
+                }
+            }
+
+            return child;
+        }
+
+        public static T GetVisualParent<T>(this Visual visual) where T : Visual
+        {
+            var p = (Visual)VisualTreeHelper.GetParent(visual);
+            var parent = p as T ?? GetVisualParent<T>(p);
+
+            return parent;
+        }
+    }
+}
