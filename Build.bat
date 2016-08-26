61e51648d1ab132488161a668aca254299c4989c
stev <stev.code@gmail.com>
2016-08-26 00:08:44 -0400

    batch build now produces deployment builds

--- Build.bat
+++ Build.bat
@@ -12,19 +12,22 @@ rem Directory paths
 set localDirectory=%~dp0
 set outputDirectory=%localDirectory%output
 set buildsDirectory=%outputDirectory%\builds
+set anyCPUBuild=%buildsDirectory%\AnyCPU
+set x64Build=%buildsDirectory%\x64
+set x86Build=%buildsDirectory%\x86
 set releasesDirectory=%outputDirectory%\releases
-set x86Release=%releasesDirectory%\x86
-set x64Release=%releasesDirectory%\x64
 set anyCPURelease=%releasesDirectory%\AnyCPU
+set x64Release=%releasesDirectory%\x64
+set x86Release=%releasesDirectory%\x86
 
 rem Clean paths
 rmdir /q /s "%outputDirectory%" 1>nul 2>nul
 
 rem Initialize paths
 mkdir "%buildsDirectory%" 1>nul 2>nul
-mkdir "%x86Release%" 1>nul 2>nul
-mkdir "%x64Release%" 1>nul 2>nul
 mkdir "%anyCPURelease%" 1>nul 2>nul
+mkdir "%x64Release%" 1>nul 2>nul
+mkdir "%x86Release%" 1>nul 2>nul
 
 rem Set build platform: Release, Debug
 rem "%~1" is the first arg
@@ -34,30 +37,35 @@ if "%~1" neq "" (set buildPlatformConfiguration=%~1)
 echo Making Classroom Learning Partner: %buildPlatformConfiguration% Builds.
 echo.
 
-echo Building x86...
-%msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="x86" /p:WarningLevel=0 /v:q /t:rebuild
+echo Building AnyCPU...
+%msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="Any CPU" /p:WarningLevel=0 /v:q /t:rebuild
 
 echo Building x64...
 %msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="x64" /p:WarningLevel=0 /v:q /t:rebuild
 
-echo Building AnyCPU...
-%msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="Any CPU" /p:WarningLevel=0 /v:q /t:rebuild
+echo Building x86...
+%msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="x86" /p:WarningLevel=0 /v:q /t:rebuild
 
 if not "%ERRORLEVEL%"=="0" (echo ERROR: Build Failed. & goto Quit)
 echo.
 
-rem echo "Copy x64"
-rem xcopy /y %outputDirectory%\x64\Release\*.pdb %x64Release% 1>nul 2>nul
-rem xcopy /y %outputDirectory%\x64\Release\*.dll %x64Release% 1>nul 2>nul
-rem xcopy /y %outputDirectory%\x64\Release\SoundSwitch.exe %x64Release% 1>nul 2>nul
-rem xcopy /y %outputDirectory%\x64\Release\SoundSwitch.exe.config %x64Release% 1>nul 2>nul
+echo Deploying AnyCPU...
+mkdir "%anyCPURelease%\lib" 1>nul 2>nul
+xcopy /y "%anyCPUBuild%\"*.dll "%anyCPURelease%\lib" 1>nul 2>nul
+xcopy /y "%anyCPUBuild%\Classroom Learning Partner.exe" "%anyCPURelease%" 1>nul 2>nul
+xcopy /y "%anyCPUBuild%\Classroom Learning Partner.exe.config" "%anyCPURelease%" 1>nul 2>nul
 
+echo Deploying x64...
+mkdir "%x64Release%\lib" 1>nul 2>nul
+xcopy /y "%x64Build%\"*.dll "%x64Release%\lib" 1>nul 2>nul
+xcopy /y "%x64Build%\Classroom Learning Partner.exe" "%x64Release%" 1>nul 2>nul
+xcopy /y "%x64Build%\Classroom Learning Partner.exe.config" "%x64Release%" 1>nul 2>nul
 
-rem echo "Copy x86"
-rem xcopy /y %outputDirectory%\x86\Release\*.pdb %x86Release% 1>nul 2>nul
-rem xcopy /y %outputDirectory%\x86\Release\*.dll %x86Release% 1>nul 2>nul
-rem xcopy /y %outputDirectory%\x86\Release\SoundSwitch.* %x86Release% 1>nul 2>nul
-rem xcopy /y %outputDirectory%\x86\Release\SoundSwitch.exe.config %x86Release% 1>nul 2>nul
+echo Deploying x86...
+mkdir "%x86Release%\lib" 1>nul 2>nul
+xcopy /y "%x86Build%\"*.dll "%x86Release%\lib" 1>nul 2>nul
+xcopy /y "%x86Build%\Classroom Learning Partner.exe" "%x86Release%" 1>nul 2>nul
+xcopy /y "%x86Build%\Classroom Learning Partner.exe.config" "%x86Release%" 1>nul 2>nul
 
 echo.
 echo All operations completed successfully.
