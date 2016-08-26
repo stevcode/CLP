656de6f8eea59e652be961b4bae60f203feba12e
stev <stev.code@gmail.com>
2016-08-25 22:06:53 -0400

    Working build batch file

--- Build.bat
+++ Build.bat
@@ -11,50 +11,39 @@ set msbuildexe="%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
 rem Directory paths
 set localDirectory=%~dp0
 set outputDirectory=%localDirectory%output
-set buildDirectory=%outputDirectory%build
-set releasesDirectory=%outputDirectory%releases
+set buildDirectory=%outputDirectory%\build
+set releasesDirectory=%outputDirectory%\releases
 set x86Release=%releasesDirectory%\x86
 set x64Release=%releasesDirectory%\x64
 set anyCPURelease=%releasesDirectory%\AnyCPU
 
 rem Clean paths
-rmdir /q /s %outputDirectory% 1>nul 2>nul
+rmdir /q /s "%outputDirectory%" 1>nul 2>nul
 
 rem Initialize paths
-mkdir %buildDirectory% 1>nul 2>nul
-mkdir %x86Release% 1>nul 2>nul
-mkdir %x64Release% 1>nul 2>nul
-mkdir %anyCPURelease% 1>nul 2>nul
-
-set finalDir=%localDirectory%Final
-
-
-git describe --abbrev=0 --tags > latestTag.txt
-for /f "delims=" %%i in ('git rev-list HEAD --count') do set commitCount=%%i
-set /p latestTag=<latestTag.txt
-del latestTag.txt
-set releaseVersion=%latestTag%.%commitCount%
+mkdir "%buildDirectory%" 1>nul 2>nul
+mkdir "%x86Release%" 1>nul 2>nul
+mkdir "%x64Release%" 1>nul 2>nul
+mkdir "%anyCPURelease%" 1>nul 2>nul
 
 rem Set build platform: Release, Debug
 rem "%~1" is the first arg
 set buildPlatformConfiguration=Release
-if "%~1" neq "" 
-(
-    set buildPlatformConfiguration=%~1
-)
+if "%~1" neq "" (set buildPlatformConfiguration=%~1)
 
 echo Making Classroom Learning Partner: %buildPlatformConfiguration% Builds.
 echo.
+
 echo Building x86...
-%msbuildexe% "Classroom Learning Partner.sln" /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="x86" /v:q /t:rebuild
+%msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="x86" /p:WarningLevel=0 /v:q /t:rebuild
+
 echo Building x64...
-%msbuildexe% "Classroom Learning Partner.sln" /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="x64" /v:q /t:rebuild
+%msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="x64" /p:WarningLevel=0 /v:q /t:rebuild
+
 echo Building AnyCPU...
-%msbuildexe% "Classroom Learning Partner.sln" /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="AnyCPU" /v:q /t:rebuild
-if not "%ERRORLEVEL%"=="0" 
-(
-    echo ERROR: Build Failed. & goto Quit
-)
+%msbuildexe% "Classroom Learning Partner.sln" /nologo /m /p:Configuration=%buildPlatformConfiguration% /p:Platform="Any CPU" /p:WarningLevel=0 /v:q /t:rebuild
+
+if not "%ERRORLEVEL%"=="0" (echo ERROR: Build Failed. & goto Quit)
 echo.
 
 rem echo "Copy x64"
