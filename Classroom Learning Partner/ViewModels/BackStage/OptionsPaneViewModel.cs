5060616727d57540be1f90477e6268bff82e37c9
stev <stev.code@gmail.com>
2016-10-26 02:03:13 -0400

    fixes

--- Classroom Learning Partner/ViewModels/BackStage/OptionsPaneViewModel.cs
+++ Classroom Learning Partner/ViewModels/BackStage/OptionsPaneViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
+using Catel.IO;
 using Catel.MVVM;
+using Catel.Windows;
+using Classroom_Learning_Partner.Services;
+using CLP.Entities;
 
 namespace Classroom_Learning_Partner.ViewModels
 {
@@ -16,6 +21,8 @@ namespace Classroom_Learning_Partner.ViewModels
         private void InitializeCommands()
         {
             GenerateRandomMainColorCommand = new Command(OnGenerateRandomMainColorCommandExecute);
+            ClearHistoryCommand = new Command(OnClearHistoryCommandExecute);
+            GenerateCommand = new Command(OnGenerateCommandExecute);
         }
 
         #endregion //Constructor
@@ -45,5 +52,67 @@ namespace Classroom_Learning_Partner.ViewModels
         }
 
         #endregion //Commands
+
+        /// <summary>SUMMARY</summary>
+        public Command ClearHistoryCommand { get; private set; }
+
+        private void OnClearHistoryCommandExecute()
+        {
+            PleaseWaitHelper.Show(() =>
+            {
+                foreach (var page in _dataService.CurrentNotebookInfo.Notebook.Pages)
+                {
+                    page.History.ClearNonAnimationHistory();
+                }
+            },
+                                  null,
+                                  "Clearing History");
+        }
+
+        /// <summary>SUMMARY</summary>
+        public Command GenerateCommand { get; private set; }
+
+        private void OnGenerateCommandExecute()
+        {
+            // HACK: This is very hardcoded.
+            if (_dataService == null ||
+                _dataService.CurrentCacheInfo == null ||
+                _dataService.CurrentNotebookInfo == null ||
+                _dataService.CurrentNotebookInfo.Notebook == null)
+            {
+                return;
+            }
+
+            var classInfoPath = Path.Combine(_dataService.CurrentCacheInfo.ClassesFolderPath, "classInfo;KK;S1nEmeKiYkSuPPo3t2nWXQ.xml");
+            var classInfo = ClassInformation.LoadFromXML(classInfoPath);
+            if (classInfo == null)
+            {
+                return;
+            }
+
+            var teacher = classInfo.Teacher;
+            var copiedNotebookT = _dataService.CurrentNotebookInfo.Notebook.CopyForNewOwner(teacher);
+            copiedNotebookT.CurrentPage = copiedNotebookT.Pages.FirstOrDefault();
+            var notebookCompositeT = NotebookNameComposite.ParseNotebook(copiedNotebookT);
+            var notebookPathT = Path.Combine(_dataService.CurrentCacheInfo.NotebooksFolderPath, notebookCompositeT.ToFolderName());
+            var notebookInfoT = new NotebookInfo(_dataService.CurrentCacheInfo, notebookPathT)
+            {
+                Notebook = copiedNotebookT
+            };
+            PleaseWaitHelper.Show(() => _dataService.SaveNotebookLocally(notebookInfoT, true), null, "Saving Notebook for " + teacher.FullName);
+
+            foreach (var person in classInfo.StudentList)
+            {
+                var copiedNotebook = _dataService.CurrentNotebookInfo.Notebook.CopyForNewOwner(person);
+                copiedNotebook.CurrentPage = copiedNotebook.Pages.FirstOrDefault();
+                var notebookComposite = NotebookNameComposite.ParseNotebook(copiedNotebook);
+                var notebookPath = Path.Combine(_dataService.CurrentCacheInfo.NotebooksFolderPath, notebookComposite.ToFolderName());
+                var notebookInfo = new NotebookInfo(_dataService.CurrentCacheInfo, notebookPath)
+                {
+                    Notebook = copiedNotebook
+                };
+                PleaseWaitHelper.Show(() => _dataService.SaveNotebookLocally(notebookInfo, true), null, "Saving Notebook for " + person.FullName);
+            }
+        }
     }
 }
