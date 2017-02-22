21cec61b4ff571d58b4fda7709e5fe02002f7940
stev <stev.code@gmail.com>
2017-02-22 10:39:09 -0500

    Updated Progress Panel resizing

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 // This file is overwritten during the build process. No changes you make will persist. 
 using System.Reflection; 
-[assembly: AssemblyVersion("17.2.22.0")] 
-[assembly: AssemblyInformationalVersion("17.2.22.0-r3988b41")] 
+[assembly: AssemblyVersion("17.2.22.2")] 
+[assembly: AssemblyInformationalVersion("17.2.22.2-ra7ad0b5")] 
