465dc3b4309f978c6cc7b957c819f9dd827aa9d4
stev <stev.code@gmail.com>
2017-03-03 11:11:39 -0500

    Release Build

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 // This file is overwritten during the build process. No changes you make will persist. 
 using System.Reflection; 
-[assembly: AssemblyVersion("17.2.22.2")] 
-[assembly: AssemblyInformationalVersion("17.2.22.2-ra7ad0b5")] 
+[assembly: AssemblyVersion("17.3.3.2")] 
+[assembly: AssemblyInformationalVersion("17.3.3.2-rfd5d136")] 
