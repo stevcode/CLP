18e2cf203f2b508cb521d4fbaf74b7f05421650b
stev <stev.code@gmail.com>
2017-03-28 18:49:05 -0400

    Smaller sample size for partial batch analysis

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 // This file is overwritten during the build process. No changes you make will persist. 
 using System.Reflection; 
-[assembly: AssemblyVersion("17.3.28.0")] 
-[assembly: AssemblyInformationalVersion("17.3.28.0-rd291ee3")] 
+[assembly: AssemblyVersion("17.3.28.1")] 
+[assembly: AssemblyInformationalVersion("17.3.28.1-r6ac3d80")] 
