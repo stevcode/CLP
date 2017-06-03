f7a4a17d8714b45f8912438b2f709473daf43469
stev <stev.code@gmail.com>
2017-06-03 00:31:34 -0400

    fixed side-matching issue

--- Classroom Learning Partner/Services/DataService.cs
+++ Classroom Learning Partner/Services/DataService.cs
@@ -82,7 +82,7 @@ namespace Classroom_Learning_Partner.Services
             //ConversionService.Stitch();
             //ConversionService.ConvertAnnCache();
             //ConvertEmilyCache();
-            AnalysisService.RunFullBatchAnalysis(AnalysisService.FixedPageNumbersToAnalyze);
+            //AnalysisService.RunFullBatchAnalysis(AnalysisService.FixedPageNumbersToAnalyze);
         }
 
         #endregion // Constructors
