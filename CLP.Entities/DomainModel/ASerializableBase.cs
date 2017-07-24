8f54997723a3c56397b5587c73c120122c4c16f9
stev <stev.code@gmail.com>
2017-07-24 19:42:59 -0400

    Cleaned up SerializableBase to only use XML instead of JSON

--- CLP.Entities/DomainModel/ASerializableBase.cs
+++ CLP.Entities/DomainModel/ASerializableBase.cs
@@ -1,98 +1,25 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Text;
+﻿using System.IO;
 using System.Xml;
 using System.Xml.Linq;
 using Catel.Data;
-using Catel.IoC;
 using Catel.Runtime.Serialization;
-using Catel.Runtime.Serialization.Json;
 using Catel.Runtime.Serialization.Xml;
-using Newtonsoft.Json;
-using Formatting = Newtonsoft.Json.Formatting;
-using JsonSerializer = Catel.Runtime.Serialization.Json.JsonSerializer;
 
 namespace CLP.Entities
 {
     public abstract class ASerializableBase : ModelBase
     {
-        private static readonly ISerializationManager SerializationManager = ServiceLocator.Default.ResolveType<ISerializationManager>();
-        private static readonly IObjectAdapter ObjectAdapter = ServiceLocator.Default.ResolveType<IObjectAdapter>();
-
-        protected ASerializableBase() { }
-
-        protected ASerializableBase(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
-
-        public string ToJsonString(bool formatWithIndents = true)
+        public string ToXmlString(XmlSerializerOptimalizationMode optimalizationMode = XmlSerializerOptimalizationMode.Performance)
         {
             using (var memoryStream = new MemoryStream())
             {
-                var configuration = new JsonSerializationConfiguration
+                var configuration = new XmlSerializationConfiguration
                                     {
-                                        Formatting = formatWithIndents ? Formatting.Indented : Formatting.None,
-                                        IsEnumSerializedWithString = true,
-                                        DateParseHandling = DateParseHandling.DateTime,
-                                        DateTimeKind = DateTimeKind.Unspecified,
-                                        DateTimeZoneHandling = DateTimeZoneHandling.Unspecified
+                                        OptimalizationMode = optimalizationMode
                                     };
 
-                var jsonSerializer = new JsonSerializer(SerializationManager, TypeFactory.Default, ObjectAdapter);
-                jsonSerializer.Serialize(this, memoryStream, configuration);
-                memoryStream.Position = 0;
-                using (var reader = new StreamReader(memoryStream))
-                {
-                    var jsonString = reader.ReadToEnd();
-                    return jsonString;
-                }
-            }
-        }
-
-        public void ToJsonFile(string filePath, bool formatWithIndents = true)
-        {
-            var jsonString = ToJsonString(formatWithIndents);
-            File.WriteAllText(filePath, jsonString);
-        }
-
-        public static T FromJsonString<T>(string json) where T : class
-        {
-            using (var memoryStream = new MemoryStream(Encoding.Default.GetBytes(json)))
-            {
-                var configuration = new JsonSerializationConfiguration
-                                    {
-                                        IsEnumSerializedWithString = true,
-                                        DateParseHandling = DateParseHandling.DateTime,
-                                        DateTimeKind = DateTimeKind.Unspecified,
-                                        DateTimeZoneHandling = DateTimeZoneHandling.Unspecified
-                                    };
-
-                var jsonSerializer = new JsonSerializer(SerializationManager, TypeFactory.Default, ObjectAdapter);
-                //try
-                //{
-                    var deserialized = jsonSerializer.Deserialize(typeof(T), memoryStream, configuration);
-                    return (T)deserialized;
-                //}
-                //catch (Exception ex)
-                //{
-                //    CLogger.AppendToLog($"Error trying to deserialize {typeof(T)} via json.\n{ex.Message}");
-                //    return null;
-                //}
-            }
-        }
-
-        public static T FromJsonFile<T>(string filePath) where T : class
-        {
-            var json = File.ReadAllText(filePath);
-            return FromJsonString<T>(json);
-        }
-
-        public string ToXmlString()
-        {
-            using (var memoryStream = new MemoryStream())
-            {
                 var xmlSerializer = SerializationFactory.GetXmlSerializer();
-                xmlSerializer.Serialize(this, memoryStream, null);
+                xmlSerializer.Serialize(this, memoryStream, configuration);
 
                 memoryStream.Position = 0L;
                 using (var xmlReader = XmlReader.Create(memoryStream))
@@ -100,6 +27,33 @@ namespace CLP.Entities
                     return XDocument.Load(xmlReader).ToString();
                 }
             }
+        }
+
+        public void ToXmlFile(string filePath, XmlSerializerOptimalizationMode optimalizationMode = XmlSerializerOptimalizationMode.Performance)
+        {
+            // Copied from personal json implementation
+            var xmlString = ToXmlString(optimalizationMode);
+            File.WriteAllText(filePath, xmlString);
+
+            // From Catel Example
+            //var fileInfo = new FileInfo(filePath);
+            //if (!Directory.Exists(fileInfo.DirectoryName))
+            //{
+            //    Directory.CreateDirectory(fileInfo.DirectoryName);
+            //}
+
+            //using (Stream stream = new FileStream(filePath, FileMode.Create))
+            //{
+            //    var configuration = new XmlSerializationConfiguration
+            //                        {
+            //                            OptimalizationMode = optimalizationMode
+            //                        };
+
+            //    var xmlSerializer = SerializationFactory.GetXmlSerializer();
+            //    xmlSerializer.Serialize(this, stream, configuration);
+
+            //    ClearIsDirtyOnAllChilds();
+            //}
         }
 
         public static T FromXmlString<T>(string xml) where T : class
@@ -116,8 +70,15 @@ namespace CLP.Entities
                 memoryStream.Position = 0L;
 
                 var xmlSerializer = SerializationFactory.GetXmlSerializer();
-                return (T) xmlSerializer.Deserialize(typeof(T), memoryStream, null);
+                var deserialized = xmlSerializer.Deserialize(typeof(T), memoryStream, null);
+                return (T) deserialized;
             }
+        }
+
+        public static T FromXmlFile<T>(string filePath) where T : class
+        {
+            var xmlString = File.ReadAllText(filePath);
+            return FromXmlString<T>(xmlString);
         }
     }
 }
