a145d3cc501489b92791d05aa659d3e62d417e1b
stev <stev.code@gmail.com>
2017-09-07 23:49:29 -0400

    Created Analysis Code Class

---
+++ CLP.Entities/DomainModel/HistoryAnalysis/AnalysisCode.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Catel.Data;
+
+namespace CLP.Entities
+{
+    public class AnalysisCode : ASerializableBase, IAnalysisCode
+    {
+        #region Nested Classes
+
+        public class AnalysisConstraint : ASerializableBase
+        {
+            #region Constructors
+
+            public AnalysisConstraint(string constraintLabel, string constraintValue)
+            {
+                ConstraintLabel = constraintLabel;
+                ConstraintValue = constraintValue;
+            }
+
+            #endregion // Constructors
+
+            #region Properties
+
+            public string ConstraintLabel
+            {
+                get => GetValue<string>(ConstraintLabelProperty);
+                set => SetValue(ConstraintLabelProperty, value);
+            }
+
+            public static readonly PropertyData ConstraintLabelProperty = RegisterProperty(nameof(ConstraintLabel), typeof(string), string.Empty);
+
+            public string ConstraintValue
+            {
+                get => GetValue<string>(ConstraintValueProperty);
+                set => SetValue(ConstraintValueProperty, value);
+            }
+
+            public static readonly PropertyData ConstraintValueProperty = RegisterProperty(nameof(ConstraintValue), typeof(string), string.Empty);
+
+            #endregion // Properties
+        }
+
+        #endregion // Nested Classes
+
+        #region Properties
+
+        public string AnalysisLabel
+        {
+            get => GetValue<string>(AnalysisLabelProperty);
+            set => SetValue(AnalysisLabelProperty, value);
+        }
+
+        public static readonly PropertyData AnalysisLabelProperty = RegisterProperty(nameof(AnalysisLabel), typeof(string), string.Empty);
+
+        /// <summary>Short-form alias of the analysis code.</summary>
+        public string Alias
+        {
+            get => GetValue<string>(AliasProperty);
+            set => SetValue(AliasProperty, value);
+        }
+
+        public static readonly PropertyData AliasProperty = RegisterProperty(nameof(Alias), typeof(string), string.Empty);
+
+        public List<AnalysisConstraint> ConstraintValues
+        {
+            get => GetValue<List<AnalysisConstraint>>(ConstraintValuesProperty);
+            set => SetValue(ConstraintValuesProperty, value);
+        }
+
+        public static readonly PropertyData ConstraintValuesProperty =
+            RegisterProperty(nameof(ConstraintValues), typeof(List<AnalysisConstraint>), () => new List<AnalysisConstraint>());
+
+        public string FormattedValue
+        {
+            get
+            {
+                var constraintValues = string.Join(" - ", ConstraintValues.Select(c => c.ConstraintValue)).ToList();
+                var bracesString = ConstraintValues.Any() ? $" {{{constraintValues}}}" : string.Empty;
+
+                return $"{Alias}{bracesString}";
+            }
+        }
+
+        #endregion // Properties
+    }
+}
---
+++ CLP.Entities/DomainModel/HistoryAnalysis/IAnalysisCode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CLP.Entities
+{
+    public interface IAnalysisCode
+    {
+        string Alias { get; set; }
+        string AnalysisLabel { get; set; }
+        List<AnalysisCode.AnalysisConstraint> ConstraintValues { get; set; }
+        string FormattedValue { get; }
+    }
+}
