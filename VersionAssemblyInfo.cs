739a85026ec46522a186cd05efb6a3587a9c171b
stev <stev.code@gmail.com>
2018-07-10 15:53:18 -0400

    Cache location now displayed on Notebook Info Pane

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 // This file is overwritten during the build process. No changes you make will persist. 
 using System.Reflection; 
-[assembly: AssemblyVersion("18.6.29.0")] 
-[assembly: AssemblyInformationalVersion("18.6.29.0-r200cf955")] 
+[assembly: AssemblyVersion("18.7.10.0")] 
+[assembly: AssemblyInformationalVersion("18.7.10.0-r6f19de91")] 
