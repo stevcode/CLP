0e8b282fc8f5396dbb5726191f068b81e3b14b21
stev <stev.code@gmail.com>
2018-07-22 17:49:47 -0400

    Version save

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 // This file is overwritten during the build process. No changes you make will persist. 
 using System.Reflection; 
-[assembly: AssemblyVersion("18.7.20.3")] 
-[assembly: AssemblyInformationalVersion("18.7.20.3-r5590ab7d")] 
+[assembly: AssemblyVersion("18.7.22.0")] 
+[assembly: AssemblyInformationalVersion("18.7.22.0-r7ced3ea0")] 
