9c4dd812f300754550fbdf0dc3c52985fac96326
stev <stev.code@gmail.com>
2018-08-20 20:52:37 -0400

    Start of ML code

---
+++ stevcode.ML/Distance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace stevcode.ML
+{
+    public static class Distance
+    {
+        /// <summary>
+        /// Calculates the Euclidean Distance in n-dimensions.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="isFastDistance">Doesn't return the square root of the total, instead just returns a value that is proportionally far away.</param>
+        /// <returns></returns>
+        public static double EuclideanDistance(List<double> a, List<double> b, bool isFastDistance)
+        {
+            var total = a.Select((t, i) => t - b[i]).Sum(difference => difference * difference);
+
+            return isFastDistance ? total : Math.Sqrt(total);
+        }
+    }
+}
