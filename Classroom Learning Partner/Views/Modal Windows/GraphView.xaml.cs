cc733911c81015dc2c1753d95ded623a11a35794
stev <stev.code@gmail.com>
2018-08-21 08:14:44 -0400

    graph window start

---
+++ Classroom Learning Partner/Views/Modal Windows/GraphView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Catel.Windows;
+
+namespace Classroom_Learning_Partner.Views
+{
+    public partial class GraphView
+    {
+        public const double WINDOW_WIDTH = 800;
+
+        public GraphView()
+            : base(DataWindowMode.Custom)
+        {
+            InitializeComponent();
+            Top = SystemParameters.FullPrimaryScreenHeight / 2 - (ActualHeight / 2);
+            Left = SystemParameters.FullPrimaryScreenWidth / 2 - (ActualWidth / 2);
+        }
+    }
+}
