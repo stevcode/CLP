032c1a2f11379cefc7f85b63c360d0452b0bcce5
stev <stev.code@gmail.com>
2018-08-26 22:33:28 -0400

    Visible scatter points for all z-axises

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 // This file is overwritten during the build process. No changes you make will persist. 
 using System.Reflection; 
-[assembly: AssemblyVersion("18.8.20.0")] 
-[assembly: AssemblyInformationalVersion("18.8.20.0-r")] 
+[assembly: AssemblyVersion("18.8.23.0")] 
+[assembly: AssemblyInformationalVersion("18.8.23.0-r")] 
